851dd4b0ac5a8a1603a6426f61d3df713acb02a1
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-03-21 20:00:22 +0800

    实操组件和认识less 和 scoped

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,16 +1,17 @@
 <template>
-	<div>
-		<Demo v-if="flag">
-			<template v-slot:header>v-if可以使组件重新渲染和销毁</template>
-		</Demo>
-		<button @click="flag = !flag">改变组件状态</button>
-	</div>
+	<Layout> </Layout>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
-import Demo from "./components/Demo1.vue"
-let flag = ref(true)
+import Layout from "./layout/index.vue"
+name: "App"
 </script>
 
-<style></style>
+<style lang="less">
+html,
+body,
+#app {
+	height: 100%;
+	overflow: hidden;
+}
+</style>
---
+++ vite-app/src/components/Demo.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<Demo1 v-if="flag">
+			<template v-slot:header>v-if可以使组件重新渲染和销毁</template>
+		</Demo1>
+		<button @click="flag = !flag">改变组件状态</button>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+import Demo1 from "./Demo1.vue"
+let flag = ref(true)
+</script>
+
+<style></style>
