3fff5674e0132c601e85e1dd107104851b410318
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-03-26 14:13:27 +0800

    unplugin-auto-import

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,4 +1,8 @@
 <template>
+	<div>
+		<button @click="appFlag = !appFlag">change appFlag</button>
+		<p>{{ appFlag }}</p>
+	</div>
 	<renderDom title="tsx标题" @tsx-click="getTsx"></renderDom>
 	<Layout> </Layout>
 </template>
@@ -6,6 +10,8 @@
 <script setup lang="ts">
 import renderDom from "./App"
 import Layout from "./layout/index.vue"
+
+let appFlag = ref<boolean>(false)
 
 const getTsx = (param: number) => {
 	console.log("getTsx=", param)
