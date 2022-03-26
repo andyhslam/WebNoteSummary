851e6b4bf055f5f3f3fa30b1932b993b0749333e
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-03-26 13:57:34 +0800

    TSX

---
+++ vite-app/src/App.tsx
@@ -0,0 +1,34 @@
+import {ref} from 'vue' 
+
+let v = ref<string>('')
+let tsxFlag = true
+let tsxArr = [1,2,3]
+
+// 泛型字面量的模式
+type Props = {
+  title: string
+}
+
+const renderDom = (props:Props, ctx:any) => {
+  return (
+    <>
+      <h3>{props.title}</h3>
+      <input type="text" v-model={v.value} />
+      <p>{v.value}</p>
+      <p v-show={tsxFlag}>景天</p>
+      <p v-show={!tsxFlag}>雪见</p>
+      {tsxFlag ?  <p>雪见</p> : <p>景天</p>}
+      {
+        tsxArr.map(item => {
+          return <div data-index={item} onClick={clickTap.bind(this, ctx)}>${item}</div>
+        })
+      }
+    </>
+  )
+}
+
+const clickTap = (ctx: any) => {
+  ctx.emit('tsx-click', 123)
+}
+
+export default renderDom
--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,9 +1,15 @@
 <template>
+	<renderDom title="tsx标题" @tsx-click="getTsx"></renderDom>
 	<Layout> </Layout>
 </template>
 
 <script setup lang="ts">
+import renderDom from "./App"
 import Layout from "./layout/index.vue"
+
+const getTsx = (param: number) => {
+	console.log("getTsx=", param)
+}
 </script>
 
 <style lang="less">
