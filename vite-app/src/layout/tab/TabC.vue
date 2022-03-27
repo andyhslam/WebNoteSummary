23941c4e485f65cdd117c8e64c3096d7ff844f16
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-03-28 00:43:02 +0800

    详解Scoped和样式 穿透

--- vite-app/src/layout/tab/TabC.vue
+++ vite-app/src/layout/tab/TabC.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="tab-c">
 		<img id="tabC" width="120" height="100" src="../../assets/tabC.jpeg" />
+		<el-badge :value="100">
+			<el-button>忍野惠</el-button>
+		</el-badge>
 	</div>
 </template>
 
@@ -12,10 +15,15 @@ useBase64({ el: "#tabC" }).then((res) => {
 })
 </script>
 
-<style lang="less" scoped>
+<style scoped lang="less">
 .tab-c {
 	background-color: rgb(0, 255, 221);
 	height: 100px;
 	border: 1px solid #ccc;
+	.el-badge {
+		:deep(span) {
+			color: #9acd32;
+		}
+	}
 }
 </style>
