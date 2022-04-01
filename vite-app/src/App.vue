9b9b4d49c1a6e3ac6e2be897098072d63189d91b
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-04-02 01:37:55 +0800

    历史记录

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-	<div class="change-router">
-		<!-- <router-link :to="{ name: 'Login' }" style="margin-right: 10px"
+	<div class="router-jump">
+		<!-- <router-link replace :to="{ name: 'Login' }" style="margin-right: 10px"
 			>Login</router-link
 		>
-		<router-link to="/register">Register</router-link>
-		<a href="/" style="margin-right: 10px">Login</a>
+		<router-link replace to="/register">Register</router-link> -->
+		<!-- <a href="/" style="margin-right: 10px">Login</a>
 		<a href="/register">Register</a> -->
-		<!-- <button @click="toPage('/')" style="margin-right: 10px">Login</button>
-		<button @click="toPage('/register')">Register</button> -->
 		<button @click="toPage('Login')" style="margin-right: 10px">
 			Login
 		</button>
 		<button @click="toPage('Register')">Register</button>
+		<button @click="next" style="margin-right: 10px">next</button>
+		<button @click="prev">prev</button>
 	</div>
 	<!-- 路由出口，路由匹配到的组件将渲染在这里 -->
 	<router-view></router-view>
@@ -33,6 +33,14 @@ const toPage = (url: string) => {
 	// router.push(url) // 1.字符串模式
 	// router.push({ path: url }) // 2.对象模式
 	router.push({ name: url }) // 3.命名式路由模式
+	// router.replace({ name: url })
+}
+const next = () => {
+	router.go(1)
+}
+const prev = () => {
+	// router.go(-1)
+	router.back()
 }
 </script>
 
