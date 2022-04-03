d32e35b5b37dfdedd020b9873d5c60ce631cf265
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-04-04 07:08:16 +0800

    文件夹改名

--- vite-app/vite.config.ts
+++ vite-app/vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
+import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), AutoImport({
     imports: ['vue'],
     dts: "src/auto-import.d.ts"
-  })]
+  })],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
