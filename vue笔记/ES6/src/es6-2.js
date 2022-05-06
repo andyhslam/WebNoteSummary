0807cdf0802dcf2ad43809f32c978fe2e41b2e22
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-05-07 01:52:04 +0800

    es6模块化

---
+++ vue笔记/ES6/src/es6-2.js
@@ -0,0 +1,23 @@
+import { a1, b1, c1 } from "./es6-1.js"
+import { a2, b2, c2 } from "./es6-1.js"
+import { a2 as a3, b2 as b3, c2 as c3 } from "./es6-1.js"
+import * as d from "./es6-1.js"
+console.log(a1)
+console.log(b1)
+console.log(c1)
+console.log("-------------------")
+console.log(a2)
+console.log(b2)
+console.log(c2)
+console.log("-------------------")
+console.log(a3)
+console.log(b3)
+console.log(c3)
+console.log("-------------------")
+console.log(d.a1)
+console.log(d.b1)
+console.log(d.c1)
+console.log("-------------------")
+console.log(d.a2)
+console.log(d.b2)
+console.log(d.c2)
--- vue笔记/ES6/src/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-import { show, addAge } from './whole'
-import * as aicoder from './whole'
-import sdk from './whole'
-import './gwj'
-show()
-addAge(10)
-show()
-aicoder.show()
-console.log('sdk :', sdk);
