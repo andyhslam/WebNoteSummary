fbc6d95ff2ef9896f552302965f1e7d4abfff719
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-05-13 00:30:32 +0800

    post请求

--- vue笔记/node-basic/04-builtin-module/03-http/01-get/server.js
+++ vue笔记/node-basic/04-builtin-module/03-http/01-get/server.js
@@ -1,19 +1,11 @@
 const http = require("http")
 const https = require("https")
-const querystring = require("querystring")
-const log4js = require("log4js")
-log4js.configure({
-	appenders: { cheese: { type: "file", filename: "cheese.log" } },
-	categories: { default: { appenders: ["cheese"], level: "error" } },
-})
-
-const logger = log4js.getLogger("cheese")
-logger.level = "debug"
+const logger = require("../../utils/log.js")
 
 // 在函数式编程里面，createServer是一个高阶函数，能够接收回调函数作为参数
 const server = http.createServer((request, response) => {
-	// const url = request.url
-	// logger.debug(url)
+	const url = request.url
+	logger.debug(url)
 
 	https.get(
 		"https://xiaomiyoupin.com/mtop/mf/resource/data/list",
