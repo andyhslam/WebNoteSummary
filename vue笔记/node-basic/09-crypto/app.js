fdbd610282facf0e03753b76e8a10de5e8eeb5fd
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-05-22 16:47:55 +0800

    内置模块-crypto-2

--- vue笔记/node-basic/09-crypto/app.js
+++ vue笔记/node-basic/09-crypto/app.js
@@ -3,9 +3,10 @@ const crypto = require("crypto")
 const password = "abc123"
 
 /**
- * 1、sha256：非对称加密；sha1：对称加密
- * 2、createHmac：非对称加密；createHash：对称加密
- * 3、crypto链式调用的三个方法依次表示：
+ * 1、hash加密不可逆
+ * 2、sha256：非对称加密；sha1：对称加密
+ * 3、createHmac：非对称加密；createHash：对称加密
+ * 4、crypto链式调用的三个方法依次表示：
  * - createHash：加密规则(使用什么加密算法)
  * - update：加密数据(要加密的字符串)
  * - digest：显示规则(返回加密后的字符串)
