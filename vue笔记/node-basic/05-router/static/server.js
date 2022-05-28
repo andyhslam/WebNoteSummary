33e0014e162e7bb9ef88f95bc060de95573a5eb1
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-05-28 14:45:11 +0800

    静态资源目录搭建

--- vue笔记/node-basic/05-router/static/server.js
+++ vue笔记/node-basic/05-router/static/server.js
@@ -8,13 +8,13 @@ const server = http.createServer(async (req, res) => {
 	const urlString = req.url
 	const filePathName = path.join(__dirname, "./public", urlString)
 	// const filePathName = path.resolve(__dirname, "./public", `.${urlString}`)
+
+	// 读取静态文件
 	const { data, mimeType } = await readStaticFile(filePathName)
 	res.writeHead(200, { "content-type": `${mimeType};charset=utf-8` })
 	res.write(data)
 	res.end()
 })
-
-// 读取静态文件
 
 // 在 8080 端口监听请求
 server.listen(8080, () => {
