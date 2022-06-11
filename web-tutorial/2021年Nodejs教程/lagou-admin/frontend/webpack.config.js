5dbe7e53ef7b54f455d549dc5ce48dce8693f139
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-06-11 17:25:02 +0800

    Node.js项目-[05]-Webpack工程化-4

--- web-tutorial/2021年Nodejs教程/lagou-admin/frontend/webpack.config.js
+++ web-tutorial/2021年Nodejs教程/lagou-admin/frontend/webpack.config.js
@@ -1,8 +1,12 @@
 const path = require("path")
+const CopyPlugin = require("copy-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
 	// 配置环境
 	mode: "development",
+	// 在开发环境调试代码
+	devtool: "source-map",
 	// 配置入口
 	entry: {
 		app: "./src/app.js",
@@ -11,5 +15,26 @@ module.exports = {
 	output: {
 		path: path.join(__dirname, "./dist"),
 		filename: "app.js",
+	},
+	// 配置插件
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, "./public/index.html"),
+			filename: "index.html",
+			inject: true,
+		}),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: "./public/*.ico",
+				},
+			],
+		}),
+	],
+	// 配置server
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		compress: true,
+		port: 9000,
 	},
 }
