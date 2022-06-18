8fa6cd99f74b9ccef64c8ecf8a95c08c1f30966d
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-06-18 13:20:10 +0800

    Node.js项目-[09]-SME-Router路由2-3

--- web-tutorial/2021年Nodejs教程/lagou-admin/frontend/src/app.js
+++ web-tutorial/2021年Nodejs教程/lagou-admin/frontend/src/app.js
@@ -1,5 +1,6 @@
 // webpack自动将es6模块化转成浏览器能够兼容的，一直兼容到底
 import indexTpl from "./views/index.art"
+import loginTpl from "./views/login.art"
 
-const html = indexTpl({})
+const html = loginTpl({})
 $("#root").html(html)
