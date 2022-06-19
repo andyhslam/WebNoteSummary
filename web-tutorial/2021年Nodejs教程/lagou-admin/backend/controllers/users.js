14f08b7bfb78b66de79f1788a1787eab2d6ede68
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-06-19 17:13:35 +0800

    Node.js项目-[15]-注册用户(后端部分)

---
+++ web-tutorial/2021年Nodejs教程/lagou-admin/backend/controllers/users.js
@@ -0,0 +1,12 @@
+// 注册用户：数据装填到模板里，渲染到请求页面上
+const signup = (req, res, next) => {
+	const { username, password } = req.body
+	res.render("success", {
+		dataObj: JSON.stringify({
+			username,
+			password,
+		}),
+	})
+}
+
+exports.signup = signup
---
+++ web-tutorial/2021年Nodejs教程/lagou-admin/backend/routes/users.js
@@ -0,0 +1,8 @@
+var express = require("express")
+var router = express.Router()
+
+const { signup } = require("../controllers/users")
+
+router.post("/signup", signup)
+
+module.exports = router
