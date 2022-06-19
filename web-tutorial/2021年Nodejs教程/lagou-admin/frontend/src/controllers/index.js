b4d6e71249c1d8ed01357eac51f9da2b72d45198
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-06-19 15:49:48 +0800

    Node.js项目-[13]-注册用户(前端部分)

--- web-tutorial/2021年Nodejs教程/lagou-admin/frontend/src/controllers/index.js
+++ web-tutorial/2021年Nodejs教程/lagou-admin/frontend/src/controllers/index.js
@@ -1,8 +1,10 @@
 import indexTpl from "../views/index.art"
 import loginTpl from "../views/login.art"
+import usersTpl from "../views/users.art"
 
 const indexHtml = indexTpl({})
 const loginHtml = loginTpl({})
+const usersHtml = usersTpl()
 
 const _handleSubmit = (router) => {
 	return (e) => {
@@ -11,10 +13,20 @@ const _handleSubmit = (router) => {
 	}
 }
 
-const index = (router) => {
-	return (req, res, next) => {
-		res.render(indexHtml)
-	}
+const _signup = () => {
+	const $btnClose = $("#users-close")
+	// 提交表单；serialize：序列化表格内容为字符串，用于 Ajax 请求。
+	const formData = $("#users-form").serialize()
+	$.ajax({
+		// api是后端接口，users是分类，signup是具体操作
+		url: "/api/users/signup",
+		type: "post",
+		data: formData,
+		success(res) {
+			console.log(res)
+		},
+	})
+	$btnClose.click()
 }
 
 // 函数柯里化：此处是函数里面返回一个函数作为路由的回调函数
@@ -26,4 +38,19 @@ const login = (router) => {
 	}
 }
 
-export { index, login }
+const index = (router) => {
+	return (req, res, next) => {
+		res.render(indexHtml)
+
+		// window resize，让页面撑满整个屏幕
+		// $(window, ".wrapper").resize()
+
+		// 填充用户列表
+		$("#users").html(usersHtml)
+
+		// 点击保存，提交表单
+		$("#users-save").on("click", _signup)
+	}
+}
+
+export { login, index }
