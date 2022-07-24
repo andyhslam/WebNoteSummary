dc4754d224aca3b79bfac268b74414680834e7a7
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-07-24 14:55:10 +0800

    Node.js项目-[61]-职位管理-添加职位-上传图片-后端-2

--- web-tutorial/2021年Nodejs教程/lagou-admin/backend/middlewares/upload.js
+++ web-tutorial/2021年Nodejs教程/lagou-admin/backend/middlewares/upload.js
@@ -1,5 +1,24 @@
 const path = require("path")
 const multer = require("multer")
-const upload = multer({ dest: path.join(__dirname, "../public/uploads") })
+const mime = require("mime")
 
+let filename = ""
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, path.join(__dirname, "../public/uploads"))
+	},
+	filename: function (req, file, cb) {
+		let ext = mime.getExtension(file.mimetype)
+		filename = file.fieldname + "-" + Date.now() + "." + ext
+		cb(null, filename)
+	},
+})
+
+const limits = {
+	fileSize: 200 * 1024,
+	files: 2,
+}
+
+const upload = multer({ storage, limits })
 module.exports = upload
