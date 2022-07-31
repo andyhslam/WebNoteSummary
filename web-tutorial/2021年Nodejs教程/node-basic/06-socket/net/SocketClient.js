f84cb6825919378cea125c346a414c47ec8799ac
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-07-31 13:41:05 +0800

    Node.js基础-[64]-Socket编成-Net模块-2

--- web-tutorial/2021年Nodejs教程/node-basic/06-socket/net/SocketClient.js
+++ web-tutorial/2021年Nodejs教程/node-basic/06-socket/net/SocketClient.js
@@ -1,17 +1,60 @@
-const net = require("net")
-const client = net.createConnection({ port: 9527 }, () => {
-	// 'connect' listener.
-	console.log("connected to server!")
-	// 往服务端发送数据
-	client.write("world!\r\n")
+// const net = require("net")
+// const client = net.createConnection({ port: 9527 }, () => {
+// 	// 'connect' listener.
+// 	console.log("connected to server!")
+// 	// 往服务端发送数据
+// 	client.write("world!\r\n")
+// })
+
+// // 客户端接收服务端发送过来的信息
+// client.on("data", (data) => {
+// 	console.log(data.toString())
+// 	// client.end()
+// })
+
+// client.on("end", () => {
+// 	console.log("disconnected from server")
+// })
+
+let net = require("net")
+const readline = require("readline")
+
+let port = 9527
+let host = "127.0.0.1"
+
+let socket = new net.Socket()
+
+socket.setEncoding = "UTF-8"
+
+socket.connect(port, host, () => {
+	socket.write("hello.")
 })
 
-// 客户端接收服务端发送过来的信息
-client.on("data", (data) => {
-	console.log(data.toString())
-	// client.end()
+socket.on("data", (msg) => {
+	console.log(msg.toString())
+	say()
 })
 
-client.on("end", () => {
-	console.log("disconnected from server")
+socket.on("error", function (err) {
+	console.log("error" + err)
 })
+
+socket.on("close", function () {
+	console.log("connection closeed")
+})
+
+const r1 = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+})
+
+function say() {
+	r1.question("请输入：\n", (inputMsg) => {
+		if (inputMsg !== "bye") {
+			socket.write(inputMsg + "\n")
+		} else {
+			socket.destroy()
+			r1.close()
+		}
+	})
+}
--- web-tutorial/2021年Nodejs教程/node-basic/06-socket/net/SocketServer.js
+++ web-tutorial/2021年Nodejs教程/node-basic/06-socket/net/SocketServer.js
@@ -1,22 +1,57 @@
+// const net = require("net")
+
+// const server = net.createServer((socket) => {
+// 	// 新加入一个用户，就会产生一个新的socket
+// 	// socket.write()是客户端对象的方法，往客户端发送数据
+// 	socket.write("hello")
+
+// 	socket.on("data", (chunk) => {
+// 		// 服务端接收客户端发送过来的数据
+// 		console.log(chunk.toString())
+// 	})
+// })
+
+// server.on("error", (err) => {
+// 	// Handle errors here.
+// 	throw err
+// })
+
+// // Grab an arbitrary unused port.
+// server.listen("9527", () => {
+// 	console.log("opened server on", server.address())
+// })
+
 const net = require("net")
 
-const server = net.createServer((socket) => {
-	// 新加入一个用户，就会产生一个新的socket
-	// 往客户端发送数据
-	socket.write("hello")
+const server = new net.createServer()
 
-	socket.on("data", (chunk) => {
-		// 服务端接收客户端发送过来的数据
-		console.log(chunk.toString())
+let clients = {}
+let clientName = 0
+
+server.on("connection", (client) => {
+	client.name = ++clientName
+	clients[client.name] = client
+
+	client.on("data", (msg) => {
+		// console.log('客户端传来：' + msg);
+		broadcast(client, msg.toString())
+	})
+
+	client.on("error", (e) => {
+		console.log("client error" + e)
+		client.end()
+	})
+
+	client.on("close", () => {
+		delete clients[client.name]
+		console.log(client.name + " 下线了")
 	})
 })
 
-server.on("error", (err) => {
-	// Handle errors here.
-	throw err
-})
+function broadcast(client, msg) {
+	for (let key in clients) {
+		clients[key].write(client.name + " 说：" + msg)
+	}
+}
 
-// Grab an arbitrary unused port.
-server.listen("9527", () => {
-	console.log("opened server on", server.address())
-})
+server.listen(9527, "localhost")
