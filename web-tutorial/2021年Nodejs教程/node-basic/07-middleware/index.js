92cfc9963db3cb0daa842b0b1956788e46e122ce
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-08-06 09:34:21 +0800

    Node.js基础-[70]-中间件机制-express

---
+++ web-tutorial/2021年Nodejs教程/node-basic/07-middleware/index.js
@@ -0,0 +1,36 @@
+// express中间件
+const middleware1 = (req, res, next) => {
+	console.log("middleware1 start")
+	next()
+}
+// const middleware2 = (req, res, next) => {
+// 	console.log("middleware2 start")
+// 	next()
+// }
+const middleware2 = (req, res, next) => {
+	console.log("middleware2 start")
+	new Promise((resolve) => {
+		setTimeout(() => resolve(), 1000)
+	}).then(() => {
+		next()
+	})
+}
+const middleware3 = (req, res, next) => {
+	console.log("middleware3 start")
+	next()
+}
+
+// 通过递归的形式，将后续中间件的执行方法传递给当前中间件，在当前中间件执行结束，通过调用 next() 方法执行后续中间件的调用。
+
+function run(req, res) {
+	// 中间件数组
+	const middlewares = [middleware1, middleware2, middleware3]
+	const next = () => {
+		const middleware = middlewares.shift()
+		if (middleware) {
+			middleware(req, res, next)
+		}
+	}
+	next()
+}
+run() // 模拟一次请求发起
