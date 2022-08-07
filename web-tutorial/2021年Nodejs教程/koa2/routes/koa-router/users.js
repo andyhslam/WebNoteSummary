fe92600a13c388590bbc3f68b6d283abe7129449
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-08-07 18:37:50 +0800

    Node.js高级-Koa2-[04-1]-请求数据获取

--- web-tutorial/2021年Nodejs教程/koa2/routes/koa-router/users.js
+++ web-tutorial/2021年Nodejs教程/koa2/routes/koa-router/users.js
@@ -5,9 +5,48 @@ const router = new Router({
 })
 // 还可以继续引入一个路由，再通过use()定义子路由，实现嵌套路由
 // user：命名路由
-router.get("user", "/signin/:id", async (ctx, next) => {
-	ctx.body = "signin"
+router.get("user", "/login/:id", async (ctx, next) => {
+	ctx.body = "login"
 	ctx.status = 301
 })
+
+router.post("/signin", async (ctx, next) => {
+	const result = await parsePostData(ctx)
+	// 浏览器v8引擎的搜索环境有URLSearchParams方法，定义在window全局对象
+	// nodejs环境也有URLSearchParams类，定义在global全局对象
+	const param = new URLSearchParams(result)
+	console.log(param.get("username"))
+	ctx.body = result
+})
+
+// 解析上下文里node原生请求的POST参数
+function parsePostData(ctx) {
+	return new Promise((resolve, reject) => {
+		try {
+			let data = ""
+			ctx.req.addListener("data", (chunk) => {
+				data += chunk
+			})
+			ctx.req.addListener("end", () => {
+				const parseData = parseQueryStr(data)
+				resolve(parseData)
+			})
+		} catch (err) {
+			reject(err)
+		}
+	})
+}
+
+// 将POST请求参数字符串解析成JSON
+function parseQueryStr(queryStr) {
+	let queryData = {}
+	const queryStrList = queryStr.split("&")
+	console.log(queryStrList)
+	for (let [index, queryStr] of queryStrList.entries()) {
+		let itemList = queryStr.split("=")
+		queryData[itemList[0]] = decodeURIComponent(itemList[1])
+	}
+	return queryData
+}
 
 module.exports = router
