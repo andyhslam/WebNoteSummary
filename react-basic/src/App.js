60c5d3be13d16716fc14cd8fd0cd15c5e7e115db
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-09-18 22:10:21 +0800

    组件-函数组件的创建和渲染

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,22 +1,16 @@
-/**
- * jsx样式控制
- * 1.行内样式
- * 2.类名样式
- */
-import "./app.css"
-const styleObj = {
-	color: "red",
-	fontSize: "20px",
+// 定义函数组件
+function HelloFn() {
+	return <div>这是我的第一个函数组件!</div>
 }
 
-const activeFlag = true
+// 定义类组件
 function App() {
 	return (
 		<div className="App">
-			<span style={styleObj}>this is span</span>
-			<span className={activeFlag ? "active" : ""}>测试类名样式</span>
+			{/* 渲染函数组件 */}
+			<HelloFn />
+			<HelloFn></HelloFn>
 		</div>
 	)
 }
-
 export default App
