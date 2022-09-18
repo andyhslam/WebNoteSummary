339beac7a98ebb37bb60cffe8861dfe7ce721f1d
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-09-18 15:06:34 +0800

    jsx-动态类名控制

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -9,11 +9,12 @@ const styleObj = {
 	fontSize: "20px",
 }
 
+const activeFlag = true
 function App() {
 	return (
 		<div className="App">
 			<span style={styleObj}>this is span</span>
-			<span className="active">测试类名样式</span>
+			<span className={activeFlag ? "active" : ""}>测试类名样式</span>
 		</div>
 	)
 }
