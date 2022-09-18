a2d16fc4159a87acf3f668c314031afc86e0cc3c
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-09-18 22:59:48 +0800

    组件-事件绑定

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,29 +1,27 @@
 import React from "react"
 
-/**
- * 函数组件的约定说明
- * 1. 函数组件的名称必须首字母大写，react内部会根据这个来判断是组件还是普通的HTML标签
- * 2. 函数组件必须有返回值，表示该组件的UI结构；如果不需要渲染任何内容，则返回null
- * 3. 组件就像HTML标签一样可以被渲染到页面中。组件表示一段结构内容，对于函数组件来说，渲染的内容就是函数的返回值
- * 4. 使用函数名称作为组件标签名称，可以成对出现也可以自闭合
- */
-
 // 定义函数组件
 function HelloFn() {
-	return <div>这是我的第一个函数组件!</div>
+	// 定义事件回调函数
+	const clickHandler = () => {
+		console.log("函数组件中的事件被触发了")
+	}
+	return (
+		// 绑定事件
+		<button onClick={clickHandler}>click me!</button>
+	)
 }
-
-/**
- * 类组件的约定说明
- * 1. 类组件名称也必须以大写字母开头
- * 2. 类组件应该继承React.Component父类，从而使用父类中提供的方法或属性
- * 3. 类组件必须提供render方法；render方法必须有返回值，表示该组件的UI结构
- */
 
 // 定义类组件
 class HelloComponent extends React.Component {
+	// 事件回调函数(标准写法可以避免this指向问题)
+	// 如此这般，回调函数中的this指向的是当前的组件实例对象
+	clickHandler = () => {
+		console.log("类组件中的事件被触发了")
+	}
 	render() {
-		return <div>这是我的第一个类组件!</div>
+		// this指向当前的组件实例
+		return <div onClick={this.clickHandler}>这是我的第一个类组件!</div>
 	}
 }
 
