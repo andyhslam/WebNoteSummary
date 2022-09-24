4759f1778be2f18ae61f94f741cbaa58ce95b51e
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-09-24 15:13:23 +0800

    组件-状态定义修改注意事项

--- react-basic/src/sample/09-组件-状态的定义使用.js
+++ react-basic/src/sample/09-组件-状态的定义使用.js
@@ -30,6 +30,15 @@ class TestComponent extends React.Component {
 	}
 }
 
+/**
+ * 总结：
+ * 1.编写组件其实就是编写原生js类或者函数
+ * 2.定义状态必须通过state实例属性的方式，提供一个对象，名称是固定的就叫做state
+ * 3.修改state中的任何属性，都不可以通过直接赋值，必须用setState方法，此方法是继承得到的。
+ * 4.这里的this关键词，很容易出现指向错误的问题；上面的写法是最推荐和最规范的，没有this指向问题
+ */
+
+// 根组件
 function App() {
 	return (
 		<div className="App">
