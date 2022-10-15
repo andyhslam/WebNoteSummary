ae70602ed62f23e1b2f6f6b750c30f78718cf7f9
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-10-15 22:09:24 +0800

    pc项目-antd安装测试使用

--- react-geek-pc/src/App.js
+++ react-geek-pc/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from "./pages/Layout"
 import Login from "./pages/Login"
+import { Button } from "antd"
 
 function App() {
 	return (
 		// 路由配置
 		<BrowserRouter>
 			<div className="App">
+				<Button type="primary">Primary Button</Button>
 				<Routes>
 					{/* 创建路由path和组件的对应关系 */}
 					<Route path="/" element={<Layout />}></Route>
