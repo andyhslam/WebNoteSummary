86ed72696fc1584c3b38509321907134ca64a329
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-10-16 11:41:13 +0800

    pc项目-登录-表单校验实现

--- react-geek-pc/src/pages/Login/index.js
+++ react-geek-pc/src/pages/Login/index.js
@@ -7,16 +7,47 @@ function Login() {
 		<div className="login">
 			<Card className="login-container">
 				<img className="login-logo" src={logo} alt="" />
-				{/* 登录表单 */}
+				{/* 登录表单：
+          1. 为 Form 组件添加 validateTrigger 属性，指定校验触发时机的集合
+          2. 为 Form.Item 组件添加 name 属性，这样表单校验才会生效 [容易忽略]
+          3. 为 Form.Item 组件添加 rules 属性，用来添加表单校验
+        */}
 				<Form
+					validateTrigger={["onBlur", "onChange"]}
 					initialValues={{
 						remember: true,
 					}}
 				>
-					<Form.Item>
+					<Form.Item
+						name="mobile"
+						rules={[
+							{
+								required: true,
+								message: "请输入手机号!",
+							},
+							{
+								pattern: /^1[3-9]\d{9}$/,
+								message: "手机号码格式不对",
+								validateTrigger: "onBlur",
+							},
+						]}
+					>
 						<Input size="large" placeholder="请输入手机号" />
 					</Form.Item>
-					<Form.Item>
+					<Form.Item
+						name="code"
+						rules={[
+							{
+								required: true,
+								message: "请输入验证码!",
+							},
+							{
+								len: 6,
+								message: "验证码6个字符",
+								validateTrigger: "onBlur",
+							},
+						]}
+					>
 						<Input size="large" placeholder="请输入验证码" />
 					</Form.Item>
 					<Form.Item name="remember" valuePropName="checked">
