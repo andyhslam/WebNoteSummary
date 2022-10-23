0bd8ab9edeab15e720546910892ef5b36cde640a
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-10-24 01:26:54 +0800

    项目打包-路由懒加载

--- react-geek-pc/src/App.js
+++ react-geek-pc/src/App.js
@@ -3,38 +3,54 @@ import {
 	Routes,
 	Route,
 } from "react-router-dom"
+import { lazy, Suspense } from "react"
 import { history } from "@/utils"
-import Login from "@/pages/Login"
-import Layout from "@/pages/Layout"
-import Home from "@/pages/Home"
-import Article from "@/pages/Article"
-import Publish from "@/pages/Publish"
 import AuthRoute from "@/components/AuthRoute.js"
+
+// 按需导入路由组件
+const Login = lazy(() => import("@/pages/Login"))
+const Layout = lazy(() => import("@/pages/Layout"))
+const Home = lazy(() => import("@/pages/Home"))
+const Article = lazy(() => import("@/pages/Article"))
+const Publish = lazy(() => import("@/pages/Publish"))
 
 function App() {
 	return (
 		// 路由配置
 		<HistoryRouter history={history}>
 			<div className="App">
-				{/* 创建路由path和组件的对应关系 */}
-				<Routes>
-					{/* 需要鉴权的路由 */}
-					<Route
-						path="/"
-						element={
-							<AuthRoute>
-								<Layout />
-							</AuthRoute>
-						}
-					>
-						{/* 二级路由默认页面 */}
-						<Route index element={<Home />}></Route>
-						<Route path="article" element={<Article />}></Route>
-						<Route path="publish" element={<Publish />}></Route>
-					</Route>
-					{/* 不需要鉴权的路由 */}
-					<Route path="/login" element={<Login />}></Route>
-				</Routes>
+				<Suspense
+					fallback={
+						<div
+							style={{
+								textAlign: "center",
+								marginTop: 200,
+							}}
+						>
+							loading...
+						</div>
+					}
+				>
+					{/* 创建路由path和组件的对应关系 */}
+					<Routes>
+						{/* 需要鉴权的路由 */}
+						<Route
+							path="/"
+							element={
+								<AuthRoute>
+									<Layout />
+								</AuthRoute>
+							}
+						>
+							{/* 二级路由默认页面 */}
+							<Route index element={<Home />}></Route>
+							<Route path="article" element={<Article />}></Route>
+							<Route path="publish" element={<Publish />}></Route>
+						</Route>
+						{/* 不需要鉴权的路由 */}
+						<Route path="/login" element={<Login />}></Route>
+					</Routes>
+				</Suspense>
 			</div>
 		</HistoryRouter>
 	)
