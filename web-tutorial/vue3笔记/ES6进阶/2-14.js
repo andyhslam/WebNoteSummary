eabd18f40e27700b2a8dc58b9ab2c1bcb02d4618
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-12-04 14:30:00 +0800

    04-创建类和生成实例

---
+++ web-tutorial/vue3笔记/ES6进阶/2-14.js
@@ -0,0 +1,27 @@
+// 创建一个明星类
+class Star {
+	/**
+	 * constructor()方法是类的构造函数(默认方法)，用于传递参数，返回实例对象；
+	 * 通过new命令生成对象实例时，自动调用该方法;
+	 * 如果没有显式定义，类内部会自动创建一个constructor()
+	 */
+	constructor(uname, age) {
+		// this指向创建的实例对象
+		this.uname = uname
+		this.age = age
+	}
+}
+
+// 利用类创建实例对象
+const ldh = new Star("刘德华", 61)
+const zxy = new Star("张学友", 60)
+console.log(ldh)
+console.log(zxy)
+
+/**
+ * 1.通过class关键字创建类, 类名我们还是习惯性定义首字母大写。
+ * 2.类里面有个constructor函数,可以接受传递过来的参数,同时返回实例对象。
+ * 3.constructor函数：只要new生成实例时，就会自动调用这个函数；如果我们不写这个函数，类也会自动生成这个函数。
+ * 4.生成实例，new不能省略。
+ * 5.最后注意语法规范, 创建类：类名后面不要加小括号；生成实例：类名后面加小括号；构造函数不需要加function。
+ */
