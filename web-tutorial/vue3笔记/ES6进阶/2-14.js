2dada9c05aa364e5cf07ec7994290aa75e5aa5e9
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-12-04 17:19:04 +0800

    04-类中添加共有方法

--- web-tutorial/vue3笔记/ES6进阶/2-14.js
+++ web-tutorial/vue3笔记/ES6进阶/2-14.js
@@ -10,6 +10,9 @@ class Star {
 		this.uname = uname
 		this.age = age
 	}
+	sing(song) {
+		console.log(this.uname + "：" + song)
+	}
 }
 
 // 利用类创建实例对象
@@ -17,6 +20,8 @@ const ldh = new Star("刘德华", 61)
 const zxy = new Star("张学友", 60)
 console.log(ldh)
 console.log(zxy)
+ldh.sing("冰雨")
+zxy.sing("李香兰")
 
 /**
  * 1.通过class关键字创建类, 类名我们还是习惯性定义首字母大写。
@@ -24,4 +29,6 @@ console.log(zxy)
  * 3.constructor函数：只要new生成实例时，就会自动调用这个函数；如果我们不写这个函数，类也会自动生成这个函数。
  * 4.生成实例，new不能省略。
  * 5.最后注意语法规范, 创建类：类名后面不要加小括号；生成实例：类名后面加小括号；构造函数不需要加function。
+ * 6.类里面所有的函数不需要写function
+ * 7.多个函数方法之间不需要添加逗号分隔
  */
