4d9dd58fb1621bdd2adc7fd37f8783993797407f
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-12-05 23:02:23 +0800

    11-面向对象tab栏-思路分析以及布局介绍

---
+++ web-tutorial/vue3笔记/ES6进阶/面向对象-tab栏案例/js/tab.js
@@ -0,0 +1,16 @@
+class Tab {
+	constructor(id) {
+		// 获取元素
+		this.main = document.querySelector(id)
+	}
+	// 1. 切换功能
+	toggleTab() {}
+	// 2. 添加功能
+	addTab() {}
+	// 3. 删除功能
+	removeTab() {}
+	// 4. 修改功能
+	editTab() {}
+}
+
+new Tab("#tab")
