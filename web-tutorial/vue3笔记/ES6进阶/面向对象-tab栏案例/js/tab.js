f841fc795645587322a7413768599bc89136479d
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-12-10 10:45:08 +0800

    12-面向对象tab栏-模块划分

--- web-tutorial/vue3笔记/ES6进阶/面向对象-tab栏案例/js/tab.js
+++ web-tutorial/vue3笔记/ES6进阶/面向对象-tab栏案例/js/tab.js
@@ -2,6 +2,20 @@ class Tab {
 	constructor(id) {
 		// 获取元素
 		this.main = document.querySelector(id)
+		this.lis = this.main.querySelectorAll("li")
+		this.sections = this.main.querySelectorAll("section")
+		this.init()
+	}
+	init() {
+		// 初始化操作(页面加载)让相关的元素(li)绑定(点击)事件
+		for (var i = 0; i < this.lis.length; i++) {
+			// 每个li的index属性值是当前li的索引号
+			this.lis[i].index = i
+			this.lis[i].onclick = function () {
+				// 获取每个li的索引号
+				console.log(this.index)
+			}
+		}
 	}
 	// 1. 切换功能
 	toggleTab() {}
