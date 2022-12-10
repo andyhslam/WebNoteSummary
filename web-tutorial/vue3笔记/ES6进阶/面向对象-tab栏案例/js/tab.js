627286682d23da6fadb8b2cf443265da688b181b
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-12-10 11:32:31 +0800

    13-面向对象tab栏-切换功能模块

--- web-tutorial/vue3笔记/ES6进阶/面向对象-tab栏案例/js/tab.js
+++ web-tutorial/vue3笔记/ES6进阶/面向对象-tab栏案例/js/tab.js
@@ -1,6 +1,8 @@
+var that
 class Tab {
 	constructor(id) {
 		// 获取元素
+		that = this
 		this.main = document.querySelector(id)
 		this.lis = this.main.querySelectorAll("li")
 		this.sections = this.main.querySelectorAll("section")
@@ -11,14 +13,24 @@ class Tab {
 		for (var i = 0; i < this.lis.length; i++) {
 			// 每个li的index属性值是当前li的索引号
 			this.lis[i].index = i
-			this.lis[i].onclick = function () {
-				// 获取每个li的索引号
-				console.log(this.index)
-			}
+			this.lis[i].onclick = this.toggleTab
 		}
 	}
 	// 1. 切换功能
-	toggleTab() {}
+	toggleTab() {
+		// console.log(this.index) 获取当前li的索引号
+		// 此处的this指向函数的调用者(li)，所以要定义一个全局变量that保存constructor里面的this
+		// 此处的that指向实例对象，因为是实例对象调用clearClass方法，所以clearClass方法里面的this就指向实例对象。
+		that.clearClass()
+		this.className = "liactive"
+		that.sections[this.index].className = "conactive"
+	}
+	clearClass() {
+		for (var i = 0; i < this.lis.length; i++) {
+			this.lis[i].className = ""
+			this.sections[i].className = ""
+		}
+	}
 	// 2. 添加功能
 	addTab() {}
 	// 3. 删除功能
