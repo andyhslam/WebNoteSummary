b6752c70b42dbb6c59e93542ab80305220723d67
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-12-11 18:22:49 +0800

    15-利用原型对象继承方法

--- web-tutorial/vue3笔记/ES6进阶/js进阶面向对象ES6/13-借用父构造函数继承属性.js
+++ web-tutorial/vue3笔记/ES6进阶/js进阶面向对象ES6/13-借用父构造函数继承属性.js
@@ -5,6 +5,11 @@ function Father(uname, age) {
 	this.uname = uname
 	this.age = age
 }
+// 3.利用原型对象继承方法
+Father.prototype.money = function () {
+	console.log(120000)
+}
+
 // 2.子构造函数
 function Son(uname, age, score) {
 	// this指向子构造函数的对象实例
@@ -12,6 +17,13 @@ function Son(uname, age, score) {
 	Father.call(this, uname, age)
 	this.score = score
 }
+/**
+ * Son.prototype = Father.prototype
+ * 这样直接赋值会有问题，如果修改子原型对象，父原型对象也会跟着一起变化。
+ */
+Son.prototype = new Father()
+// 如果利用对象的形式修改了原型对象，别忘了利用constructor指回原来的构造函数。
+Son.prototype.constructor = Son
 
 var son = new Son("刘德华", 17, 100)
 console.log(son)
