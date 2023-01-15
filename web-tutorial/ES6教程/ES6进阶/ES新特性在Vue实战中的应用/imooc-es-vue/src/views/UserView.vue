14dc0519197884d3b05e2ec7064928f02feadd83
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-01-16 07:04:12 +0800

    获取用户信息列表

---
+++ web-tutorial/ES6教程/ES6进阶/ES新特性在Vue实战中的应用/imooc-es-vue/src/views/UserView.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="user-view">
+		<h2>用户信息</h2>
+		<ul>
+			<li v-for="item of userList" :key="item.id">{{ item.name }}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+	name: "UserView",
+	components: {},
+	data() {
+		return {
+			userList: [],
+		}
+	},
+	async created() {
+		const { data } = await axios.get(
+			"http://jsonplaceholder.typicode.com/users"
+		)
+		this.userList = data
+	},
+}
+</script>
+
+<style>
+li {
+	list-style: none;
+}
+</style>
