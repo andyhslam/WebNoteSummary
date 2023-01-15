ba7041d12ca533a2952fcb42c5541e20f43659bb
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-01-16 07:43:38 +0800

    使用Proxy代理用户信息

--- web-tutorial/ES6教程/ES6进阶/ES新特性在Vue实战中的应用/imooc-es-vue/src/views/UserView.vue
+++ web-tutorial/ES6教程/ES6进阶/ES新特性在Vue实战中的应用/imooc-es-vue/src/views/UserView.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="user-view">
 		<h2>用户信息</h2>
+		<button @click="asc">升序</button>
+		<button @click="desc">降序</button>
+		<button @click="reset">重置</button>
 		<ul>
 			<li v-for="item of userList" :key="item.id">{{ item.name }}</li>
 		</ul>
@@ -21,12 +24,53 @@ export default {
 		const { data } = await axios.get(
 			"http://jsonplaceholder.typicode.com/users"
 		)
-		this.userList = data
+		// 代理
+		this.proxy = new Proxy(
+			{},
+			{
+				get(target, key) {
+					if (key === "asc") {
+						// 升序
+						return []
+							.concat(data)
+							.sort((a, b) => (a.name > b.name ? 1 : -1))
+					} else if (key === "desc") {
+						// 降序
+						return []
+							.concat(data)
+							.sort((a, b) => (a.name < b.name ? 1 : -1))
+					} else {
+						// 重置
+						console.log(data)
+						return data
+					}
+				},
+				set() {
+					// 不允许设置
+					return false
+				},
+			}
+		)
+		this.userList = this.proxy.default
+	},
+	methods: {
+		asc() {
+			this.userList = this.proxy.asc
+		},
+		desc() {
+			this.userList = this.proxy.desc
+		},
+		reset() {
+			this.userList = this.proxy.default
+		},
 	},
 }
 </script>
 
 <style>
+ul {
+	padding: 0;
+}
 li {
 	list-style: none;
 }
