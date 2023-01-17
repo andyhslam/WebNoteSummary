815d40ab2e2d4a6b557c14f8e441f6623f682e8e
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-01-17 19:32:07 +0800

    webpack配置

---
+++ web-tutorial/ES6教程/ES6进阶/ES新特性在Vue实战中的应用/imooc-webpack/README.md
@@ -0,0 +1,11 @@
+### webpack 中插件(plugin)的作用
+
+-   打包的优化
+-   资源的管理
+-   注入环境变量
+-   html-webpack-plugin 的作用：为 html 文件引入外部资源文件，生成 html 的入口。
+-   copy-webpack-plugin 的作用：把文件或者文件夹原封不动的拷贝过去，不经过 webpack 打包编译。
+
+### npx webpack 中 npx 的作用
+
+-   调用项目内部安装的模块，不去全局里面找。
---
+++ web-tutorial/ES6教程/ES6进阶/ES新特性在Vue实战中的应用/imooc-webpack/webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "index.js",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+		}),
+		new CopyPlugin([
+			{
+				from: "static",
+				to: "static",
+			},
+		]),
+	],
+}
