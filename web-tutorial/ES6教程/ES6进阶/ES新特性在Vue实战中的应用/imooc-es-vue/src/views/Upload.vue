9be951c6673844a03cbff63d7179e2febf19b695
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-01-17 10:04:15 +0800

    异步操作多张图片上传云存储

--- web-tutorial/ES6教程/ES6进阶/ES新特性在Vue实战中的应用/imooc-es-vue/src/views/Upload.vue
+++ web-tutorial/ES6教程/ES6进阶/ES新特性在Vue实战中的应用/imooc-es-vue/src/views/Upload.vue
@@ -1,6 +1,11 @@
 <template>
 	<div class="upload-wrap">
-		<label for="upload" class="choose-img">选择图片上传</label>
+		<label
+			for="upload"
+			class="choose-img"
+			:class="{ upLoading: isUploading }"
+			>选择图片上传</label
+		>
 		<input
 			ref="file"
 			id="upload"
@@ -8,18 +13,39 @@
 			accept="image/*"
 			multiple
 			style="display: none"
+			:disabled="isUploading"
 			@change="onChange"
 		/>
 		<p class="tip">
 			提示：一次可选择多张图片，最多不超过9张（单张图片大小 &lt; 1M）
 		</p>
+		<ul class="img-container">
+			<li
+				v-for="(item, index) of imgList"
+				:key="index"
+				:style="{ background: `url(${item}) no-repeat center/contain` }"
+			></li>
+		</ul>
 	</div>
 </template>
 
 <script>
+import OSS from "ali-oss"
 export default {
 	name: "Upload",
 	components: {},
+	data() {
+		return {
+			imgList: [], // 存放上传完成的图片列表
+			isUploading: false, // 当前图片是否正在上传
+			client: new OSS({
+				region: "oss-cn-hongkong",
+				bucket: "andy-imooc-es",
+				accessKeyId: "LTAI5tGWtBGJYWAvgCAvikzf",
+				accessKeySecret: "dy5dIP8ZczWoq91yaUNS1JI4aOdq3H",
+			}),
+		}
+	},
 	methods: {
 		onChange() {
 			const newFiles = this.$refs?.file?.files
@@ -39,7 +65,37 @@ export default {
 			this.uploadFilesByOSS(files)
 		},
 		// 上传多图到阿里云OSS
-		uploadFilesByOSS(files) {},
+		uploadFilesByOSS(files) {
+			this.isUploading = true
+			const uploadRequest = []
+			for (const file of files) {
+				const singlePromise = new Promise((resolve, reject) => {
+					this.client
+						.put(`${Math.random()}-${file.name}`, file)
+						.then((res) => {
+							resolve(res.url)
+						})
+						.catch((err) => {
+							reject(err)
+						})
+				})
+				uploadRequest.push(singlePromise)
+			}
+			Promise.allSettled(uploadRequest)
+				.then((res) => {
+					const imgs = []
+					for (const item of res) {
+						if (item.status === "fulfilled") {
+							imgs.push(item.value)
+						}
+					}
+					this.imgList = imgs
+					this.isUploading = false
+				})
+				.catch((err) => {
+					console.log(err)
+				})
+		},
 	},
 }
 </script>
@@ -57,5 +113,16 @@ export default {
 }
 .tip {
 	color: #ccc;
+}
+.img-container > li {
+	list-style: none;
+	width: 120px;
+	height: 100px;
+	float: left;
+	margin: 0 30px 30px 0;
+	border: 1px solid #ccc;
+}
+.upLoading {
+	background-color: #ccc;
 }
 </style>
