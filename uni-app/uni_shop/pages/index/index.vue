aa222a32b1c51ab885e4a9242abf79c24b8534d8
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-01-29 23:28:01 +0800

    28-处理patch请求

--- uni-app/uni_shop/pages/index/index.vue
+++ uni-app/uni_shop/pages/index/index.vue
@@ -13,17 +13,20 @@
 		},
 		 async onLoad() {
 			// const res = await this.$u.get('/api/index')
-			this.$u.post('/api/auth/wx/bind').then(res => {
-				// Promise.then的写法不影响后面代码的执行，所以不需要使用try...catch(e)来捕获异常
-				console.log('res-bind', res);
-			})
-			try {
-				const res = await this.$u.post('/api/auth/login')
-				console.log('res-login', res);
-				// async...await的写法影响后面代码的执行，所以需要使用try...catch(e)来捕获异常
-			} catch(e) {
-				console.log(e);
-			}
+			// this.$u.post('/api/auth/wx/bind').then(res => {
+			// 	// Promise.then的写法不影响后面代码的执行，所以不需要使用try...catch(e)来捕获异常
+			// 	console.log('res-bind', res);
+			// })
+			// try {
+			// 	const res = await this.$u.post('/api/auth/login')
+			// 	console.log('res-login', res);
+			// 	// async...await的写法影响后面代码的执行，所以需要使用try...catch(e)来捕获异常
+			// } catch(e) {
+			// 	console.log(e);
+			// }
+			
+			const res = await this.$u.patch('/api/orders/1/confirm')
+			console.log(res);
 		},
 		methods: {
 
