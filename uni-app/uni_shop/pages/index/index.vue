67c1326bd1b36c4c27f30c3bb7b1d291d0961cbf
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-02-05 22:48:50 +0800

    36-优化加载中的体验

--- uni-app/uni_shop/pages/index/index.vue
+++ uni-app/uni_shop/pages/index/index.vue
@@ -2,18 +2,19 @@
 	<view class="wrap">
 		<u-swiper :list="slides" name="img_url" height="320"></u-swiper>
 		<u-tabs :list="sortList" :is-scroll="false" :current="currentSort" @change="changeSort"></u-tabs>
-		<u-row gutter="16">
-			<u-col span="6" v-for="item in goods">
+		<u-row gutter="16"  class="u-skeleton">
+			<u-col span="6" v-for="(item, index) in goods.length ? goods : 4" :key="index">
 				<navigator class="goods-item u-m-t-30 u-p-40">
-					<u-image width="100%" height="300rpx" :src="item.cover_url"></u-image>
-					<view class="title u-m-10 u-font-32 u-line-1">{{item.title}}</view>
+					<u-image class="u-skeleton-fillet" width="100%" height="300rpx" :src="item.cover_url"></u-image>
+					<view class="title u-m-10 u-font-32 u-line-1 u-skeleton-rect">{{item.title || '商品名称'}}</view>
 					<view class="u-flex u-row-between">
-						<view class="price">￥ {{item.price}}</view>
-						<view class="sales">销量：{{item.sales}}</view>
+						<view class="price u-skeleton-rect">￥ {{item.price}}</view>
+						<view class="sales u-skeleton-rect">销量：{{item.sales}}</view>
 					</view>
 				</navigator>
 			</u-col>
 		</u-row>
+		<u-skeleton :loading="loading" :animation="true" bgColor="#FFF"></u-skeleton>
 	</view>
 </template>
 
@@ -31,6 +32,7 @@
 				slides: [],
 				goods: [],
 				page: 1,
+				loading: false,
 			}
 		},
 		onLoad() {
@@ -50,6 +52,7 @@
 				this.getIndexData()
 			},
 			async getIndexData() {
+				this.loading = true
 				const params = {
 					page: this.page
 				}
@@ -58,6 +61,7 @@
 				if(this.currentSort === 2) params.recommend = 1
 				if(this.currentSort === 3) params.new = 1
 				const res = await this.$u.api.index(params)
+				this.loading = false
 				console.log('res', res);
 				this.slides = res.slides
 				// this.goods.push(...res.goods.data)
@@ -71,12 +75,15 @@
 	.goods-item {
 		box-shadow: 0 12rpx 20rpx 0 rgba(0, 0, 0, .1);
 		.title {
+			width: 100%;
 			font-weight: 500;
 		}
 		.price {
+			width: 40%;
 			color: red;
 		}
 		.sales {
+			width: 50%;
 			color: #888;
 		}
 	}
