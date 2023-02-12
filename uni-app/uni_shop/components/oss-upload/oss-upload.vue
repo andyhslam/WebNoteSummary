500ae47d4d9ddee23f5674b979de7c8679b2f0e6
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-02-13 00:09:35 +0800

    50-头像上传组件

---
+++ uni-app/uni_shop/components/oss-upload/oss-upload.vue
@@ -0,0 +1,38 @@
+<template>
+	<u-upload 
+		:action="action" 
+		:max-size="5 * 1024 * 1024"
+		 max-count="1"
+		:before-upload="beforeUpload"
+		:custom-btn="true"
+		:form-data="formData"
+		:multiple="false"
+		:deletable="false"
+		width="0"
+		height="0"
+	>
+		<u-avatar slot="addBtn" :src="vuex_user.avatar_url" size="140"></u-avatar>
+	</u-upload>
+</template>
+
+<script>
+	export default {
+		name: "oss-upload",
+		data() {
+			return {
+				action: '',
+				formData: {},
+			};
+		},
+		methods: {
+			// 上传前的钩子
+			beforeUpload(index, list) {
+				return true
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- uni-app/uni_shop/pages/center/index.vue
+++ uni-app/uni_shop/pages/center/index.vue
@@ -1,8 +1,8 @@
 <template>
 	<view>
 		<view class="u-flex user-box u-p-l-30 u-p-r-20 u-p-t-30 u-p-b-30">
-			<view class="u-m-r-10">
-				<u-avatar :src="vuex_user.avatar_url" size="140"></u-avatar>
+			<view class="u-m-r-10 avatar-body">
+				<oss-upload></oss-upload>
 			</view>
 			<view class="u-flex-1">
 				<view class="u-font-18 u-p-b-20">{{vuex_user.name}}</view>
@@ -83,5 +83,9 @@ page{
 }
 .user-box{
 	background-color: #fff;
+}
+.avatar-body{
+	width: 70px;
+	height: 70px;
 }
 </style>
