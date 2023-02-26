8bd2d3545456b85d0f9a785c55892ef5893732aa
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-02-26 22:31:59 +0800

    2-4创建BrowserWindow

--- electron/electron-quick-start/renderer.js
+++ electron/electron-quick-start/renderer.js
@@ -5,3 +5,7 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('node-version').innerHTML = process.versions.node
+})
