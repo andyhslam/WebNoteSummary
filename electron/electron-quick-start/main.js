f766662e680ca3e0eb8057df5b5044a6aad7a2a4
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-02-26 22:52:49 +0800

    2-5进程间的通信

--- electron/electron-quick-start/main.js
+++ electron/electron-quick-start/main.js
@@ -1,14 +1,16 @@
 const { app, BrowserWindow } = require('electron')
 
 app.on('ready', () => {
+  require('devtron').install()
   let mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1600,
+    height: 1200,
     webPreferences: {
       nodeIntegration: true,
     }
   })
   mainWindow.loadFile('index.html')
+  mainWindow.webContents.openDevTools()
 
   let secondWindow = new BrowserWindow({
     width: 400,
