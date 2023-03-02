119960589d6e76f0e10d5488cc71dfec2b7dc56e
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-03-02 23:03:41 +0800

    003-第一个例子

---
+++ vue-project/vue3-basic/src/views/01-getting-started/Demo.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+console.log(count)
+const handleClick = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <div>
+    {{ count }}
+    <button @click="handleClick">add</button>
+  </div>
+</template>
+
+<style lang="less"></style>
