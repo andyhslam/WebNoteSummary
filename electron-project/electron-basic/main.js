b8330226e24a7d47d47179828f39261a67e78079
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-03-11 21:56:05 +0800

    029-App+BrowserWindow

--- electron-project/electron-basic/main.js
+++ electron-project/electron-basic/main.js
@@ -1,10 +1,27 @@
 const path = require('path')
+// 因为electron-win-state没有对nodejs做exports的暴露，所以要在require后面加上default
+const WinState = require('electron-win-state').default
 const { app, BrowserWindow, ipcMain } = require('electron')
 
+const winState = new WinState({
+  x: 200,
+  y: 200,
+  defaultWidth: 800,
+  defaultHeight: 600,
+})
+
 const createWindow = () => {
+  // electron.BrowserWindow: 创建和控制浏览器窗口
   const win = new BrowserWindow({
+    x: 100,
+    y: 100,
     width: 1000,
     height: 800,
+    show: false,
+    // frame: false,
+    backgroundColor: '#f4f4f4',
+    // 在window系统也没显示右上角的标题栏
+    titleBarOverlay: true,
     webPreferences: {
       // 集成node环境
       // nodeIntegration: true,
@@ -12,36 +29,91 @@ const createWindow = () => {
       // contextIsolation: false,
       // 主进程与渲染进程隔离，可以通过预加载的方式调用Node模块
       preload: path.resolve(__dirname, './preload.js')
-    }
+    },
+    ...winState.winOptions,
   })
-  // win.loadURL('http://jx.1000phone.net/')
+  winState.manage(win)
+  // win.loadURL('https://www.electronjs.org/')
   win.loadFile('index.html')
-  // 打开开发者工具
-  win.webContents.openDevTools()
+
+  // webContents是BrowserWindow对象的一个属性，负责渲染和控制网页。
+  const wc = win.webContents
+  // wc.openDevTools()
+  wc.on('did-finish-load', () => {
+    // 在主进程监测，渲染进程的页面的所有资源是否加载完毕
+    console.log('Content fully loaded')
+  })
+  wc.on('dom-ready', () => {
+    // 在主进程监测，渲染进程的dom节点(即html元素)是否准备完毕
+    console.log('DOM Ready')
+  })
+  wc.on('new-window', (e, url) => {
+    // 在主进程监测，渲染进程是否打开新窗口
+    // 阻止默认行为：可以阻止打开新窗口
+    // e.preventDefault()
+    console.log('new window open')
+  })
+
+  wc.on('context-menu', (e, params) => {
+    // 在主进程监测，渲染进程的右键上下文信息
+    console.log(`Image Url: ${params.srcURL}`)
+    console.log(`Selection can be copied: ${params.editFlags.canCopy}`)
+    // 往渲染进程的页面注入一段JS代码
+    wc.executeJavaScript(`alert('${params.selectionText}')`)
+  })
+
+  // 当页面加载完毕，再显示浏览器窗口
+  win.on('ready-to-show', () => {
+    win.show()
+  })
   // 暂时关闭安全警告
   // process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+
+  // const win2 = new BrowserWindow({
+  //   width: 600,
+  //   height: 400,
+  // 定义父窗口
+  //   parent: win,
+  // 锁定在主窗口
+  //   modal: true,
+  // })
+  // win2.loadURL('http://jx.1000phone.net/')
 }
+
+// 在 browserWindow 失去焦点时触发
+app.on('browser-window-blur', (e) => {
+  console.log('App blur')
+  setTimeout(() => {
+    // 关闭应用程序
+    app.quit()
+  }, 3000)
+})
+
+// 在 browserWindow 获得焦点时发出
+app.on('browser-window-focus', (e) => {
+  console.log('App focused')
+})
 
 app.on('window-all-closed', () => {
   console.log('window-all-closed')
-  // 对于 MacOS 系统 -> 关闭窗口时，不能直接退出应用
+  /**
+   * 默认情况下，一旦关闭所有窗口，就会直接退出应用；
+   * 但是对于MacOS系统，关闭窗口时，不能直接退出应用
+   */
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-app.on('quit', () => {
-  console.log('quit')
+// 在应用程序开始关闭窗口之前触发
+app.on('before-quit', (e) => {
+  console.log('App is quiting')
+  // 阻止默认行为：可以阻止应用程序的关闭
+  e.preventDefault()
 })
 
-app.whenReady().then(() => {
-  createWindow()
-  // 在MacOS下，当全部窗口关闭，点击 dock 图标，窗口再次打开。
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+app.on('quit', () => {
+  console.log('quit')
 })
 
 /**
@@ -50,5 +122,21 @@ app.whenReady().then(() => {
  * 否则，监听器的返回值(此处是msg)将被用来作为应答值。
  */
 ipcMain.handle('send-event', (event, msg) => {
-  return msg
+  const desktopPath = app.getPath('desktop')
+  return { msg, desktopPath }
+})
+
+app.whenReady().then(() => {
+  // 获取主进程的各个应用的路径
+  console.log(app.getPath('desktop'))
+  console.log(app.getPath('music'))
+  console.log(app.getPath('temp'))
+  console.log(app.getPath('userData'))
+  createWindow()
+  // 在MacOS下，当全部窗口关闭，点击dock图标，窗口再次打开。
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
 })
--- electron-project/electron-basic/preload.js
+++ electron-project/electron-basic/preload.js
@@ -1,15 +1,20 @@
-// const fs = require('fs')
-// fs.writeFile('/Users/Andyt/Desktop/test.txt', 'abc', () => {
-//   console.log('done.')
-// })
-
 // 预加载脚本中只能使用渲染进程的相关模块
 const { contextBridge, ipcRenderer } = require('electron')
+const fs = require('fs')
+const path = require('path')
+
+fs.writeFile('C:\\Users\\Andyt\\Desktop\\女士的品格.txt', '万茜', () => {
+  console.log('女一号')
+})
 
 // 在渲染进程接收主进程传递过来的内容，只能用异步操作去接收
 const handleSend = async () => {
-  const fallback = await ipcRenderer.invoke('send-event', 'kitty')
+  const fallback = await ipcRenderer.invoke('send-event', '女一变女二')
   console.log('fallback', fallback)
+  const txtPath = path.join(fallback.desktopPath, '女士的品格.txt')
+  fs.writeFile(txtPath, '邢菲', () => {
+    console.log(fallback.msg)
+  })
 }
 
 // contextBridge作为一座桥梁，把主进程的Node模块注入到myApi接口，并且将myApi挂载到渲染进程的全局对象window
