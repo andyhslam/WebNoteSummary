98f6fee4f30dc951f0239d72fab4ba541ace9029
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-04-03 00:45:00 +0800

    ts-函数类型

--- vue-project/type-script/src/view/sample/function-type.ts
+++ vue-project/type-script/src/view/sample/function-type.ts
@@ -4,19 +4,37 @@ const func1 = function (name: string, age?: number, id: number = 10): string {
 }
 console.log(func1('lx', 20))
 
-// 用接口去约束函数的参数和返回值
+// 用接口去约束函数的参数(为对象)和返回值
 interface User {
   name: string
   age: number
 }
-const func3 = function (user: User): User {
+const func2 = function (user: User): User {
   return user
 }
-let khp = func3({
+let khp = func2({
   name: 'khp',
   age: 30,
 })
-// console.log(khp);
+console.log(khp)
+
+interface Obj {
+  list: number[]
+  add1?(): void
+  add2: (this: Obj, num: number) => void
+  add3?: () => null
+}
+
+// ts可以定义this的类型，这在js中无法使用(因为js中的this无法当作参数使用)；必须是第一个参数定义this的类型。
+let obj2: Obj = {
+  list: [1, 2, 3],
+  add2(this: Obj, num: number) {
+    this.list.push(num)
+  },
+}
+// 函数传参时，忽略第一个参数(this)，把num当作第一个参数
+obj2.add2(4)
+console.log('obj2', obj2)
 
 // 函数重载：
 // 1、重载是方法名字相同，而参数不同，返回类型可以相同也可以不同。
