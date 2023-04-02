7d1be3b5dbd3d92883e9417ae7c7ca2cc18e199e
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-04-02 20:50:58 +0800

    ts-数组类型

--- vue-project/type-script/src/view/sample/array-type.ts
+++ vue-project/type-script/src/view/sample/array-type.ts
@@ -3,9 +3,10 @@ let arr0: (number | string)[] = [1, 2, '3']
 let arr1: number[] = [1, 2, 3]
 let arr2: string[] = ['1', '2', '3']
 let arr3: boolean[] = [false, true]
-let arr4: any[] = [1, '2', true]
+let arr4: any[] = [1, '2', true, {}]
+let arr04: Object[] = [1, '2', true, {}]
 // 声明多维数组
-let arr5: number[][][] = [[[]], [[]], [[]]]
+let arr5: number[][][] = [[[1]], [[]], [[3]]]
 
 // 泛型方式声明
 let arr6: Array<number> = [1, 2, 3]
@@ -32,6 +33,13 @@ function Arr(...args: number[]): void {
   console.log(arr10 === arguments) // true
 }
 Arr(4, 5, 6)
+
+// 使用interface定义对象数组
+interface ObjArr {
+  name: string
+  age?: number
+}
+const arr01: ObjArr[] = [{ name: 'zyn' }, { name: 'khp' }]
 
 // 用接口表示数组：一般用来描述类数组
 interface ArrNumber {
