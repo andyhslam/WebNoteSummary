21706ca28792b8c24ffdfef64ca5fdca2a548be6
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-04-03 23:55:14 +0800

    ts-联合类型

--- vue-project/type-script/src/view/sample/united-type.ts
+++ vue-project/type-script/src/view/sample/united-type.ts
@@ -1,36 +1,34 @@
 // 联合类型
 let phone: number | string = '213243'
-let fn1 = function (type: number | boolean): boolean {
-  return !!type;
+let fn01 = function (type: string | number | boolean): boolean {
+  return !!type
 }
-let result = fn1(false);
-console.log(result);
-
+let result01 = fn01(' ')
+console.log(result01)
 
 // 交叉类型：不能多也不能少
 interface People {
-  name: string,
+  name: string
   age: number
 }
 interface Manzj {
-  sex: string,
+  sex: string
 }
-const lxy = (man:Manzj & People): void => {
-  console.log(man);
+const lxy = (man: Manzj & People): void => {
+  console.log(man)
 }
 lxy({
   name: 'lxy',
   age: 26,
   sex: 'female',
-});
+})
 
-
-// 类型断言：不能乱用
+// 类型断言：只能欺骗ts编译器,无法避免运行时的错误,所以不能滥用.
 let fn2 = function (num: number | string): void {
   // 类型断言写法一
-  console.log((num as string).length);
+  console.log((num as string).length)
 }
-fn2('12345');
+fn2('12345')
 
 interface Au {
   run: string
@@ -40,16 +38,17 @@ interface Bu {
 }
 let fn3 = (type: Au | Bu): void => {
   // 类型断言写法二
-  console.log((<Au>type).run);
+  console.log((<Au>type).run)
 }
 fn3({
-  build: '123'
-});
+  build: '123',
+})
 
 // window.abc = 13; // 这样写是错的
-// (window as any).abc = 13; // 在any类型的变量上，访问任何属性都是允许的；可以使用any类型去做临时断言。
+;(window as any).abc = 13
+//在any类型的变量上，访问任何属性都是允许的；任何变量都可以被断言成any类型,可以使用any类型去做临时断言。
 
 const fn4 = (type: any): boolean => {
-  return type as boolean;
+  return type as boolean
 }
-console.log(fn4(1));
+console.log(fn4(1))
