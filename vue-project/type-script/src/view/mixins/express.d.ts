40e515304a79fff8e49ba9db2e316776f6f25713
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-04-09 22:52:05 +0800

    ts-声明文件

--- vue-project/type-script/src/view/mixins/express.d.ts
+++ vue-project/type-script/src/view/mixins/express.d.ts
@@ -1,1 +1,35 @@
-// declare var express: () => any;
+// 手写express的声明文件
+
+// 扩充模块
+declare module 'express' {
+  interface Router {
+    get(path: string, cb: (req: any, res: any) => void): void
+  }
+  interface App {
+    use(path: string, router: any): void
+    listen(port: number, cb?: () => void)
+  }
+  interface Express {
+    (): App
+    Router(): Router
+  }
+  const express: Express
+  export default express
+}
+
+// 扩充全局变量
+declare let expnum: number
+
+// 扩充函数
+declare function expfun(params: type) {}
+
+// 扩充类
+declare class ExpCls {}
+
+// 扩充枚举
+declare enum ExpEnum {
+  a = 1,
+  b = 2,
+}
+
+// 还可以扩充全局的(global)、命名空间(namespace)等等
