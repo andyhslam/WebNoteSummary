bc8593a75403d49f24edb1008cc80eb8905cfba3
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-04-15 14:15:21 +0800

    组合继承

--- web-tutorial/ES6教程/ES6进阶/2-12.js
+++ web-tutorial/ES6教程/ES6进阶/2-12.js
@@ -1,5 +1,5 @@
 // 父类
-function Animal(name) {
+function Animal (name) {
 	this.name = name
 }
 Animal.prototype.showName = function () {
@@ -7,7 +7,7 @@ Animal.prototype.showName = function () {
 }
 
 // 子类
-function Dog(name, color) {
+function Dog (name, color) {
 	/**
 	 * 对于构造函数继承，只能继承父类的属性，不能继承父类的方法；
 	 * call的第一个参数指的是，你要把this指向谁，就把要指向的那个对象作为第一个参数；
@@ -18,8 +18,11 @@ function Dog(name, color) {
 }
 // 对于原型继承，能够继承父类的方法，此时不需要传参
 Dog.prototype = new Animal()
-// console.log(Dog.prototype.__proto__ === Animal.prototype)
-console.log(Dog.prototype.constructor === Animal.prototype.constructor)
+console.log(Dog.prototype.__proto__ === Animal.prototype) // true
+console.log(Dog.prototype.__proto__.constructor === Animal.prototype.constructor) // true
+console.log(Dog.prototype.constructor === Animal.prototype.constructor) // true
+console.log(Dog.prototype.__proto__.constructor === Dog.prototype.constructor) // true
+console.log(Dog.prototype.__proto__ === Dog.prototype) // false
 Dog.prototype.constructor = Dog // 修正constructor的指向
 // console.log(Dog.prototype.__proto__.constructor)
 const d1 = new Dog("wangcai", "white")
