b3b2ea596c5ffa1a3b230bdff7f7c596837c00f1
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-04-22 10:47:08 +0800

    ts27-infer类型提取

--- vue-project/type-script/src/view/sample/infer-type.ts
+++ vue-project/type-script/src/view/sample/infer-type.ts
@@ -13,3 +13,28 @@ type INFER<T> = T extends Array<infer U> ? U : never
 type tuple = [string, number]
 type union = INFER<tuple>
 type bool = INFER<boolean>
+
+/**
+ * 1.提取头部元素:
+ * 类型参数 T 通过extends约束，只能是数组类型，然后通过infer声明局部one变量做提取，
+ * 后面的元素可以是任意类型，然后把局部变量返回
+ */
+type Arr = ['a', 'b', 'c']
+// 泛型工具
+type First<T extends any[]> = T extends [infer one, ...any[]] ? one : []
+type firstOne = First<Arr>
+
+// 2.提取尾部元素
+type Last<T extends any[]> = T extends [...any[], infer three] ? three : []
+type lastOne = Last<Arr>
+
+/**
+ * 3.剔除第一个元素 shift
+ * 除了第一个的元素，把其他的剩余元素声明成一个变量直接返回，就可以剔除第一个元素
+ */
+type Shift<T extends any[]> = T extends [unknown, ...infer Rest] ? Rest : []
+type shiftArr = Shift<Arr>
+
+// 4.剔除尾部元素 pop
+type Pop<T extends any[]> = T extends [...infer Rest, unknown] ? Rest : []
+type popArr = Pop<Arr>
