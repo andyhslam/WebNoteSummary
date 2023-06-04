c4cabafca3738c3a5e8133fc0b7816caa98af953
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-06-04 15:36:12 +0800

    工程模式（1）

---
+++ web-tutorial/design-pattern/server/index.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+// process.cwd()：获取项目的根路径；__dirname：获取当前路径。
+// app.use('/', express.static(path.join(process.cwd(), './modules')))
+app.use('/', express.static(path.resolve(__dirname, '../modules')))
+console.log(process.cwd())
+console.log(__dirname)
+
+app.listen(3333)
