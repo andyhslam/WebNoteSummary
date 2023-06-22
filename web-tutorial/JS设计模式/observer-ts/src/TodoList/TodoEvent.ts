06e22a9d784c96e009c5bf0c4e4db49367141255
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-06-22 09:30:04 +0800

    观察者模式（2）

---
+++ web-tutorial/JS设计模式/observer-ts/src/TodoList/TodoEvent.ts
@@ -0,0 +1,45 @@
+import { ITodo } from '.'
+
+class TodoEvent {
+  private static instance: TodoEvent
+  private todoData: ITodo[] = []
+
+  public static create() {
+    if (!TodoEvent.instance) {
+      TodoEvent.instance = new TodoEvent()
+    }
+    return TodoEvent.instance
+  }
+
+  public addTodo(todo: ITodo) {
+    return new Promise((resolve, reject) => {
+      const _todo: ITodo | undefined = this.todoData.find(
+        (t) => t.content === todo.content
+      )
+      if (_todo) {
+        alert('该项已存在')
+        return reject(1001)
+      }
+      this.todoData.push(todo)
+      resolve(todo)
+    })
+  }
+  public removeTodo(id: number) {
+    return new Promise((resolve, reject) => {
+      this.todoData = this.todoData.filter((t) => t.id !== id)
+      resolve(id)
+    })
+  }
+  public toggleTodo(id: number) {
+    return new Promise((resolve, reject) => {
+      this.todoData.forEach((t) => {
+        if (t.id === id) {
+          t.completed = !t.completed
+          resolve(id)
+        }
+      })
+    })
+  }
+}
+
+export default TodoEvent
