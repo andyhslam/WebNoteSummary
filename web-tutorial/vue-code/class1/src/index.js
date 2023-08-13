5b47324426140566e561762c93973f02d3d0033f
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-08-13 10:28:40 +0800

    【Vue2.0】数据劫持（2）

--- web-tutorial/vue-code/class1/src/index.js
+++ web-tutorial/vue-code/class1/src/index.js
@@ -1,3 +1,21 @@
 import Vue from 'vue'
 
-console.log(11)
+let vm = new Vue({
+  el: '#app',
+  data () {
+    return {
+      title: '学生列表',
+      classNum: 1,
+      total: 2,
+      teacher: ['Miss Wang', 'Miss Lee'],
+      students: [
+        { id: 1, name: 'Jack' },
+        { id: 2, name: 'Tom' },
+      ]
+    }
+  }
+})
+
+console.log(vm.title)
+console.log(vm._data.title)
+console.log(vm.$options.data().title)
--- web-tutorial/vue-code/class1/vue/index.js
+++ web-tutorial/vue-code/class1/vue/index.js
@@ -1,1 +1,16 @@
-console.log('vue')
+import { initState } from './init.js'
+
+function Vue (options) {
+  // 实例化Vue时，就要执行_init方法
+  this._init(options)
+
+}
+
+// 在初始化Vue实例时，就已经完成数据劫持
+Vue.prototype._init = function (options) {
+  var vm = this
+  vm.$options = options
+  initState(vm)
+}
+
+export default Vue
