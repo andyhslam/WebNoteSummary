6d01e656270292d3791509d619c6fa1ab271886c
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-08-27 09:37:13 +0800

    【Vue2.0】模板编译（7）

--- web-tutorial/vue-code/class2/src/vdom/vnode.js
+++ web-tutorial/vue-code/class2/src/vdom/vnode.js
@@ -3,7 +3,7 @@ function createElement (tag, attrs = {}, ...children) {
 }
 
 function createTextVnode (text) {
-  return vnode(null, null, null, text)
+  return vnode(undefined, undefined, undefined, text)
 }
 
 function vnode (tag, props, children, text) {
