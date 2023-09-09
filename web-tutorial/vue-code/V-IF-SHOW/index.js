f4848769b1688593a46adb6f714924a9943fcdcb
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-09-09 19:45:22 +0800

    【前端造轮子-3】源码（2）【不依赖Vue数据劫持】

--- web-tutorial/vue-code/V-IF-SHOW/index.js
+++ web-tutorial/vue-code/V-IF-SHOW/index.js
@@ -7,5 +7,33 @@
  */
 
 class Nicola {
+  constructor(options) {
+    const { el, data, methods } = options
+    this.el = document.querySelector(el)
+    this.data = data
+    this.methods = methods
+    this.showPool = new Map()
+    this.eventPool = new Map()
 
+    this.init()
+  }
+
+  init () {
+    this.initData()
+  }
+
+  initData () {
+    for (let key in this.data) {
+      Object.defineProperty(this, key, {
+        get () {
+          console.log('访问：', key, this.data[key])
+          return this.data[key]
+        },
+        set (newValue) {
+          this.data[key] = newValue
+        },
+      })
+    }
+    console.log(this)
+  }
 }
