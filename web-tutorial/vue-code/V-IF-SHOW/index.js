8adb32e5278be702d6fb5ab4c559867b084fdf53
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-09-09 20:21:57 +0800

    【前端造轮子-3】源码（3）【不依赖Vue数据劫持】

--- web-tutorial/vue-code/V-IF-SHOW/index.js
+++ web-tutorial/vue-code/V-IF-SHOW/index.js
@@ -1,6 +1,4 @@
 /**
- * 1.代理数据与数据劫持
- * 2.初始化DOM
  * 3.初始化视图
  * 4.事件处理函数的绑定
  * 5.改变数据的同时，改变DOM
@@ -20,8 +18,12 @@ class Nicola {
 
   init () {
     this.initData()
+    this.initDom(this.el)
+
+    console.log(this.showPool, this.eventPool)
   }
 
+  // 1.代理数据与数据劫持
   initData () {
     for (let key in this.data) {
       Object.defineProperty(this, key, {
@@ -34,6 +36,43 @@ class Nicola {
         },
       })
     }
-    console.log(this)
   }
+
+  // 2.初始化DOM
+  initDom (el) {
+    const _childNodes = el.childNodes
+
+    if (!_childNodes.length) {
+      return
+    }
+
+    _childNodes.forEach((dom) => {
+      // 判断是否为元素节点
+      if (dom.nodeType === 1) {
+        const vIf = dom.getAttribute('v-if')
+        const vShow = dom.getAttribute('v-show')
+        const vClickEvent = dom.getAttribute('@click')
+
+        if (vIf) {
+          this.showPool.set(dom, {
+            type: 'if',
+            show: this.data[vIf],
+            data: vIf
+          })
+        } else if (vShow) {
+          this.showPool.set(dom, {
+            type: 'show',
+            show: this.data[vShow],
+            data: vShow
+          })
+        }
+
+        if (vClickEvent) {
+          this.eventPool.set(dom, this.methods[vClickEvent])
+        }
+      }
+      this.initDom(dom)
+    })
+  }
+
 }
