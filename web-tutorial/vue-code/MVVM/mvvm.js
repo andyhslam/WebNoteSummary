3319ecc415f5de7127a54108e0567fec3db164e9
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-09-09 12:55:08 +0800

    【前端造轮子-2】源码（2）【不依赖Vue数据劫持】

--- web-tutorial/vue-code/MVVM/mvvm.js
+++ web-tutorial/vue-code/MVVM/mvvm.js
@@ -5,5 +5,26 @@ class MVVM {
     this.init()
   }
 
-  init () { }
+  init () {
+    this.initData()
+  }
+
+  initData () {
+    const _this = this
+    this.data = {}
+    for (let key in this._data) {
+      Object.defineProperty(this.data, key, {
+        get () {
+          console.log('获取数据：', key, _this._data[key])
+          return _this._data[key]
+        },
+        set (newValue) {
+          console.log('设置数据：', key, newValue)
+          _this._data[key] = newValue
+        },
+      })
+    }
+    this.data.age
+    this.data['name'] = '奥本海默'
+  }
 }
