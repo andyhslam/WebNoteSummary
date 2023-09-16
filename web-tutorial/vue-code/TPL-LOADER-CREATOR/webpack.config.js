ab46b21d378caaa1e6de5881394795871edb8778
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-09-16 08:28:37 +0800

    【前端造轮子-4】loader（1）

---
+++ web-tutorial/vue-code/TPL-LOADER-CREATOR/webpack.config.js
@@ -0,0 +1,48 @@
+/**
+ * mode: development、production
+ * entry 入口文件
+ * output: path、filename 打包输出路径
+ * devtool: source-map
+ * module: rules、loader
+ * plugins 插件
+ * devServer 开发服务器
+ */
+
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: resolve(__dirname, 'src/app.js'),
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'bundle.[hash:16].js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tpl$/,
+        // tpl先处理得到的字符串，再交给babel转成js程序，打包到bundle.js；
+        // webpack的loader的执行顺序是从后到前的。
+        use: [
+          'babel-loader',
+          {
+            loader: 'tpl-loader',
+            options: {
+              log: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'index.html')
+    })
+  ],
+  devServer: {
+    port: 3333
+  }
+}
