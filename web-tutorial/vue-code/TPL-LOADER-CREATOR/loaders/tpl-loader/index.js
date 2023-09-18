7a8334679e313888116d48b5bdd38e31d9b27dbb
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-09-19 07:33:09 +0800

    【前端造轮子-4】loader（5）

--- web-tutorial/vue-code/TPL-LOADER-CREATOR/loaders/tpl-loader/index.js
+++ web-tutorial/vue-code/TPL-LOADER-CREATOR/loaders/tpl-loader/index.js
@@ -1,12 +1,18 @@
 const { tplReplace } = require('../utils')
+const { getOptions } = require('loader-utils')
 
 function tplLoader (source) {
   source = source.replace(/\s+/g, '')
+  const { log } = getOptions(this)
   console.log('source', source)
+
+  // this.resourcePath：当前编译的文件的路径
+  const _log = log ? `console.log('compiled the file which is from ${this.resourcePath}')` : ''
 
   return `
     export default (options) => {
       ${tplReplace.toString()}
+      ${_log.toString()}
       return tplReplace('${source}', options)
     }
   `
