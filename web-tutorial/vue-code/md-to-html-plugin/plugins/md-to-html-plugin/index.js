69f30c2ec4b58f83ddc63d91426d95b41283a5ba
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-09-23 08:22:19 +0800

    【前端造轮子-5】plugin（2）

--- web-tutorial/vue-code/md-to-html-plugin/plugins/md-to-html-plugin/index.js
+++ web-tutorial/vue-code/md-to-html-plugin/plugins/md-to-html-plugin/index.js
@@ -1,3 +1,7 @@
+const { readFileSync } = require('fs')
+const { resolve } = require('path')
+
+const INNER_MARK = '<!-- inner -->'
 
 class MdToHtmlPlugin {
   constructor({ template, filename }) {
@@ -6,6 +10,29 @@ class MdToHtmlPlugin {
     }
     this.template = template
     this.filename = filename || 'md.html'
+  }
+
+  apply (compiler) {
+    // 第一个参数是插件名称
+    compiler.hooks.emit.tap('md-to-html-plugin', (compilation) => {
+      const _assets = compilation.assets
+      // 读取test.md文件的内容
+      const _mdContent = readFileSync(this.template, 'utf8')
+      const _templateHTML = readFileSync(resolve(__dirname, 'template.html'), 'utf8')
+      const _mdContentArr = _mdContent.split('\n')
+      const _htmlStr = compileHTML(_mdContentArr)
+      const _finalHTML = _templateHTML.replace(INNER_MARK, _htmlStr)
+
+      // 添加一个资源到test.html
+      _assets[this.filename] = {
+        source () {
+          return _finalHTML
+        },
+        size () {
+          return _finalHTML.length
+        }
+      }
+    })
   }
 }
 
