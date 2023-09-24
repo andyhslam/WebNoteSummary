eac1eec7ca850153bebc0a246e93996dd9962b3d
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-09-24 12:14:57 +0800

    【前端造轮子-5】plugin（5）

--- web-tutorial/vue-code/md-to-html-plugin/plugins/md-to-html-plugin/compiler.js
+++ web-tutorial/vue-code/md-to-html-plugin/plugins/md-to-html-plugin/compiler.js
@@ -1,6 +1,7 @@
 
 const reg_mark = /^(.+?)\s/
 const reg_sharp = /^\#/
+const reg_crossbar = /^\-/
 
 function createTree (mdArr) {
   let _htmlPool = {}
@@ -11,6 +12,8 @@ function createTree (mdArr) {
     if (matched) {
       const mark = matched[1]
       const input = matched['input']
+
+      // 判断h标签
       if (reg_sharp.test(mark)) {
         const tag = `h${mark.length}`
         const tagContent = input.replace(reg_mark, '').replace(/\r/, '')
@@ -21,6 +24,22 @@ function createTree (mdArr) {
           _lastMark = mark
           _htmlPool[tag] = {
             type: 'single',
+            tags: [`<${tag}>${tagContent}</${tag}>`]
+          }
+        }
+      }
+
+      // 判断无序列表：ul标签
+      if (reg_crossbar.test(mark)) {
+        const tag = 'li'
+        const tagContent = input.replace(reg_mark, '').replace(/\r/, '')
+        // 当前标签和上一个标签是一样的，表示他们是一组的
+        if (reg_crossbar.test(_lastMark)) {
+          _htmlPool['ul'].tags.push(`<${tag}>${tagContent}</${tag}>`)
+        } else {
+          _lastMark = mark
+          _htmlPool['ul'] = {
+            type: 'wrap',
             tags: [`<${tag}>${tagContent}</${tag}>`]
           }
         }
