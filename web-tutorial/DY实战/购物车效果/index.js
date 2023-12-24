37618fba17832576571a9078e5613a245ac313c6
andyhslam <42641473+andyhslam@users.noreply.github.com>
2023-12-24 23:12:03 +0800

    04、实战-购物车效果

---
+++ web-tutorial/DY实战/购物车效果/index.js
@@ -0,0 +1,124 @@
+// 单件商品的数据
+class UIGoods {
+  constructor(g) {
+    this.data = g
+    this.choose = 0
+  }
+
+  // 获取总价
+  getTotalPrice () {
+    return this.data.price * this.choose
+  }
+
+  // 是否选中了此件商品
+  isChoose () {
+    return this.choose > 0
+  }
+
+  // 选择的数量+1
+  increase () {
+    this.choose++
+  }
+
+  // 选择的数量-1
+  decrease () {
+    if (this.choose === 0) {
+      return
+    }
+    this.choose--
+  }
+}
+
+// 整个界面的数据
+class UIData {
+  constructor() {
+    const uiGoods = []
+    for (let i = 0; i < goods.length; i++) {
+      const uig = new UIGoods(goods[i])
+      uiGoods.push(uig)
+    }
+    this.uiGoods = uiGoods
+    this.deliveryThreshold = 30
+    this.deliveryPrice = 5
+  }
+
+  getTotalPrice () {
+    let sum = 0
+    for (let i = 0; i < this.uiGoods.length; i++) {
+      const g = this.uiGoods[i]
+      sum += g.getTotalPrice()
+    }
+    return sum
+  }
+
+  // 增加某件商品的选中数量
+  increase (index) {
+    this.uiGoods[index].increase()
+  }
+
+  // 减少某件商品的选中数量
+  decrease (index) {
+    this.uiGoods[index].decrease()
+  }
+
+  // 得到总共的选择数量
+  getTotalChooseNumber () {
+    let sum = 0
+    for (let i = 0; i < this.uiGoods.length; i++) {
+      sum += this.uiGoods[i].choose
+    }
+    return sum
+  }
+
+  // 购物车中有没有东西
+  hasGoodsInCar () {
+    return this.getTotalChooseNumber() > 0
+  }
+
+  // 是否跨过了配送标准
+  isCrossDeliveryThreshold () {
+    return this.getTotalPrice() >= this.deliveryThreshold
+  }
+}
+
+// 整个界面
+class UI {
+  constructor() {
+    this.uiData = new UIData()
+    this.doms = { goodsContainer: document.querySelector('.goods-list') }
+    this.createHTML()
+  }
+
+  // 根据商品数量创建商品列表元素
+  createHTML () {
+    let html = ''
+    for (let i = 0; i < this.uiData.uiGoods.length; i++) {
+      const g = this.uiData.uiGoods[i]
+      html += `<div class="goods-item">
+        <img src="${g.data.pic}" class="goods-pic">
+        <div class="goods-info">
+          <h2 class="goods-title">${g.data.title}</h2>
+          <p class="goods-desc">${g.data.desc}</p>
+          <p class="goods-sell">
+            <span>月售 ${g.data.sellNumber}</span>
+            <span>好评率 ${g.data.favorRate}%</span>
+          </p>
+          <div class="goods-confirm">
+            <p class="goods-price">
+              <span class="goods-price-unit">￥</span>
+              <span>${g.data.price}</span>
+            </p>
+            <div class="goods-btns">
+              <i class="iconfont i-jianhao"></i>
+              <span>${g.choose}</span>
+              <i class="iconfont i-jiajianzujianjiahao"></i>
+            </div>
+          </div>
+        </div>
+      </div>`
+    }
+    this.doms.goodsContainer.innerHTML = html
+  }
+}
+
+const ui = new UI()
