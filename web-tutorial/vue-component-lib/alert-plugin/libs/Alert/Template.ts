da85baa4b803d45842919beecbff5cd6ef87a8fd
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-01-11 00:15:36 +0800

    2_完成视图样式编写、完成显示逻辑

---
+++ web-tutorial/vue-component-lib/alert-plugin/libs/Alert/Template.ts
@@ -0,0 +1,25 @@
+import { IAlertOptions } from './typing'
+import './style/index.css'
+
+class Template {
+  // 该方法只提供给子类
+  protected alertView(options: IAlertOptions) {
+    const { headerTitle, alertText } = options
+    const oAlert: HTMLElement = document.createElement('div')
+    oAlert.className = 'alert hide'
+    oAlert.innerHTML = `
+      <div class="inner">
+        <header class="alert-header">
+          <h1>${headerTitle}</h1>
+          <span class="icon">&times;</span>
+        </header>
+        <div class="alert-wrap">
+          <p>${alertText}</p>
+        </div>
+      </div>
+    `
+    return oAlert
+  }
+}
+
+export default Template
--- web-tutorial/vue-component-lib/alert-plugin/libs/Alert/index.ts
+++ web-tutorial/vue-component-lib/alert-plugin/libs/Alert/index.ts
@@ -1,12 +1,28 @@
+import Template from './Template'
 import { IAlertOptions } from './typing'
 
-class Alert {
+class Alert extends Template {
   private _headerTitle: string
   private _alertText: string
+  private _oAlert: HTMLElement
 
+  // 实例化Alert，就会执行其构造器
   constructor(options: IAlertOptions) {
+    // 调用父类的构造器
+    super()
     this._headerTitle = options.headerTitle || 'This is my Alert'
     this._alertText = options.alertText || 'This is my Alert content'
+    this.render()
+  }
+
+  private render() {
+    document.body.appendChild(
+      this.alertView({
+        headerTitle: this._headerTitle,
+        alertText: this._alertText,
+      })
+    )
+    this._oAlert = document.querySelector('.alert') as HTMLElement
   }
 
   public static create(options: IAlertOptions) {
@@ -14,7 +30,7 @@ class Alert {
   }
 
   public show(type: string, options: IAlertOptions) {
-    console.log(type, options)
+    this._oAlert.className = 'alert show'
   }
 }
 
