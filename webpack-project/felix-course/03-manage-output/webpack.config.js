beb964ed94776901d2dec8e70c2539de0f87bac0
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-01-21 21:39:33 +0800

    1.3.2-使用HtmlWebpackPlugin

---
+++ webpack-project/felix-course/03-manage-output/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'app.html',
+      inject: 'body'
+    })
+  ]
+}
