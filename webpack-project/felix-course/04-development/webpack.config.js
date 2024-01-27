e901cf8e53368b66b1757889a45c2d7c42647226
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-01-27 22:39:24 +0800

    1.4.4-使用webpack-dev-server

--- webpack-project/felix-course/04-development/webpack.config.js
+++ webpack-project/felix-course/04-development/webpack.config.js
@@ -16,5 +16,11 @@ module.exports = {
       filename: 'app.html',
       inject: 'body'
     })
-  ]
+  ],
+  // 其实webpack-dev-server并没有输出任何物理文件，它把输出的打包以后的bundle文件放到内存里；
+  // 即使删掉dist文件夹，也能正常访问浏览器。
+  devServer: {
+    // 告知devServer从什么位置查找文件，即devServer指向的物理路径
+    static: './dist', // 将当前服务的根目录指向到dist文件夹
+  }
 }
