4684e9ad358509e655c7892e16bb4448b934043f
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-02-14 12:03:51 +0800

    2.2.2-模块解析(resolve)

--- webpack-project/advanced-course/02-modules-dependence/01-resolver/webpack.config.js
+++ webpack-project/advanced-course/02-modules-dependence/01-resolver/webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
       '@': path.resolve(__dirname, './src'),
     },
     // 识别扩展名的顺序从左到右
+    // webpack会按照数组顺序去解析这些后缀名，对于同名的文件，webpack总是会先解析列在数组首位的后缀名的文件。
     extensions: ['.js', '.json', '.vue'],
   },
 };
