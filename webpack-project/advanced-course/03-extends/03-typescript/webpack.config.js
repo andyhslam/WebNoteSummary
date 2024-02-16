5d43bad22a5dd9a2e699c1a64ea6c589d43c0ae6
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-02-16 11:53:02 +0800

    2.3.3-TypeScript

---
+++ webpack-project/advanced-course/03-extends/03-typescript/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    clean: true,
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
