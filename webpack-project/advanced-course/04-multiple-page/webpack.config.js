419edf01b11582ea079222589b987aa51750f4a3
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-02-16 19:50:04 +0800

    2.4.2-index.html模板配置

--- webpack-project/advanced-course/04-multiple-page/webpack.config.js
+++ webpack-project/advanced-course/04-multiple-page/webpack.config.js
@@ -2,8 +2,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      /**
+       * ejs模板的语法：
+       * <%= htmlWebpackPlugin.options.title %> 或者 <%- htmlWebpackPlugin.options.title %>
+       */
+      title: '多页面应用2',
+      template: './index.html',
+      // 将生成的script或者link标签注入到body元素中
+      inject: 'body',
+      // 指定当前页面该打包入口的哪些chunk
+      chunks: ['main', 'main2', 'lodashModule'],
+      // 多个页面或者模板分别载入不同的chunk
+    }),
+  ],
   entry: {
+    // 入口的每一项就是一个chunk
     main: {
       import: ['./src/app2.js', './src/app.js'],
       dependOn: 'lodashModule',
