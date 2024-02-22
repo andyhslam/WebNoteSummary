a47a44f15c957fe54e6648f470b8f1ab1ee291fe
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-02-23 05:10:55 +0800

    2.7.1-Shimming预置全局变量

--- webpack-project/advanced-course/07-shimming/01-third-party-shimming/src/index.js
+++ webpack-project/advanced-course/07-shimming/01-third-party-shimming/src/index.js
@@ -1,3 +1,4 @@
 // import _ from 'lodash';
 
 console.log(_.join(['hello', 'webpack'], ' '));
+console.log(join(['hello', '斑马'], ' '));
--- webpack-project/advanced-course/07-shimming/01-third-party-shimming/webpack.config.js
+++ webpack-project/advanced-course/07-shimming/01-third-party-shimming/webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
        * 如果webpack看到模块中用到这个变量，它将在最终bundle中引入给定的package。
        */
       _: 'lodash',
+      // 还可以使用ProvidePlugin暴露出某个模块中单个导出，通过配置一个“数组路径”实现此功能。
+      // 无论join方法在何处调用，我们都只会获取到lodash中提供的join方法。
+      // 这样就能很好的与tree shaking配合，将lodash library中的其余没有用到的导出去除。
+      join: ['lodash', 'join'],
     }),
   ],
 };
