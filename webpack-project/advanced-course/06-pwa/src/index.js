240470e3454af6c37502be2e17147b065e05ad84
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-02-23 03:56:12 +0800

    2.6.3-注册Service Worker

--- webpack-project/advanced-course/06-pwa/src/index.js
+++ webpack-project/advanced-course/06-pwa/src/index.js
@@ -1,1 +1,15 @@
 console.log('hello webpack');
+
+// serviceWorker要配合webpack和浏览器，来达到浏览器端模拟原生APP的交互效果，即使网络断开，也能访问页面的静态内容
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('SW 注册成功', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW 注册失败', registrationError);
+      });
+  });
+}
