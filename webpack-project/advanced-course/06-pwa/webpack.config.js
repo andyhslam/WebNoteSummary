49598f0dc7846a3dd89d17c7de157e2759c42f21
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-02-23 03:16:19 +0800

    2.6.2-添加Workbox

--- webpack-project/advanced-course/06-pwa/webpack.config.js
+++ webpack-project/advanced-course/06-pwa/webpack.config.js
@@ -1,9 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const workboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new workboxPlugin.GenerateSW({
+      // 快速启用 ServiceWorkers
+      clientsClaim: true,
+      // 不允许遗留任何“旧的” ServiceWorkers
+      skipWaiting: true,
+    }),
+  ],
   devServer: {
     // 开发环境的server的中间件
     devMiddleware: {
