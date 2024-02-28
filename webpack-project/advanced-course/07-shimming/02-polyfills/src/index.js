636a97e31c8a4d2fd6fb2e7413031e9475e6088b
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-02-29 00:22:23 +0800

    2.7.5-进一步优化polyfills

--- webpack-project/advanced-course/07-shimming/02-polyfills/src/index.js
+++ webpack-project/advanced-course/07-shimming/02-polyfills/src/index.js
@@ -1,3 +1,10 @@
-import '@babel/polyfill';
+/**
+ * When setting `useBuiltIns: 'usage'`, polyfills are automatically imported when needed.
+  Please remove the direct import of `@babel/polyfill` or use `useBuiltIns: 'entry'` instead.
+  `@babel/polyfill` is deprecated. Please, use required parts of `core-js`
+  and `regenerator-runtime/runtime` separately
+  已经在配置文件里配置@babel/preset-env，就不要再全局引入整个polyfill包。
+ */
+// import '@babel/polyfill';
 // 这个电片(即@babel/polyfill)一旦载入进来，就会在执行下面这句代码之前，把所有可能需要降级的代码放在以下代码的前面。
 console.log(Array.from([1, 2, 3], (x) => x + x));
--- webpack-project/advanced-course/07-shimming/02-polyfills/webpack.config.js
+++ webpack-project/advanced-course/07-shimming/02-polyfills/webpack.config.js
@@ -4,4 +4,37 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: ['last 1 version', '> 1%'],
+                  useBuiltIns: 'usage',
+                  // 由于@babel/polyfill在7.4.0中被弃用，我们建议直接添加corejs并通过corejs选项设置版本。
+                  corejs: 3,
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
+
+/**
+ * useBuiltIns: 参数有 “entry”、”usage”、false 三个值
+ * 默认值是 false ，此参数决定了babel打包时如何处理@babel/polyfilll 语句。
+ * “entry”: 会将文件中 import @babel/polyfilll 语句 结合 targets ，转换为一系列引入语句，去掉目标浏览器已支持的 polyfilll 模块，不管代码里有没有用到，只要目标浏览器不支持都会引入对应的 polyfilll 模块。
+ * “usage”: 不需要手动在代码里写 import @babel/polyfilll ，打包时会自动根据实际代码的使用情况，结合 targets 引入代码里实际用到部分 polyfilll 模块。
+ * false: 对 import‘@babel/polyfilll’不作任何处理，也不会自动引入 polyfilll 模块。
+ * 需要注意的是在 webpack 打包文件配置的 entry 中引入的 @babel/polyfill 不会根据useBuiltIns 配置作任何转换处理。
+ */
