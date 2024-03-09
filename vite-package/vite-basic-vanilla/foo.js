ae0ba9aa0dcb14f17309a5fb4d9b6c5005e3cab5
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-03-09 23:42:48 +0800

    06-03-模块热重载-HMR-API-2

--- vite-package/vite-basic-vanilla/foo.js
+++ vite-package/vite-basic-vanilla/foo.js
@@ -1,3 +1,30 @@
 export const foo = () => {
-  console.log('foo works!');
+  console.log('foo works!!!！');
 };
+
+let cache = {
+  amount: 0,
+};
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => {
+    // 在模块热更新时，dispose会记录上一个模块清理的动作，与此同时，我们可以做一些清理副作用的操作，例如清理上一个定时器。
+    if (timer) {
+      clearInterval(timer);
+    }
+  });
+
+  // 让cache缓存模块上一个状态的amount
+  // 连续赋值的规则：最终值先赋值给第一个变量，再赋值给第二个变量
+  cache = import.meta.hot.data.cache = {
+    // 这里的data可以缓存模块在状态发生变化时，一个公共的变量或者是全局的状态
+    amount: import.meta.hot.data.cache ? import.meta.hot.data.cache.amount : 0,
+  };
+}
+
+let timer = setInterval(() => {
+  cache.amount++;
+  console.log(cache.amount);
+}, 1000);
+
+export { cache };
--- vite-package/vite-basic-vanilla/main.js
+++ vite-package/vite-basic-vanilla/main.js
@@ -3,11 +3,25 @@ import { foo } from './foo.js';
 
 foo();
 
+console.log('main module.');
+
 if (import.meta.hot) {
   // 监测其它模块(foo.js)的变化
   import.meta.hot.accept('./foo.js', (newModule) => {
     // newModule：当前被监测的模块的引用
     // 当foo模块再次被更新的时候
-    newModule.foo();
+    if (newModule.cache.amount > 5) {
+      // 刷新页面
+      console.log('invalidate貌似不支持刷新页面');
+      import.meta.hot.invalidate();
+    } else {
+      // 执行热更新
+      newModule.foo();
+    }
   });
+  // 当前文件发生变化时，执行它的热更新
+  import.meta.hot.accept();
+  // 让当前模块刷新页面，不执行热替换
+  // import.meta.hot.decline();
+  console.log('decline貌似也不支持刷新页面');
 }
