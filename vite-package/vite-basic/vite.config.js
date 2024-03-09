83376e54a6588b98bff9c633f3c595dcc1e66111
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-03-09 18:47:46 +0800

    06-01-模块热重载-常用框架集成

--- vite-package/vite-basic/vite.config.js
+++ vite-package/vite-basic/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  optimizeDeps: {
+    include: [],
+  },
+  // 缓存的目标文件夹，把服务器端的文件缓存到.cache文件夹
+  cacheDir: './.cache',
+});
