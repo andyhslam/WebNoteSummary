50848b1dce484ef951f15154f4ed983f7ed0a71a
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-03-10 23:53:34 +0800

    11-Vite中使用WebWorker

--- vite-package/vite-basic-vanilla2/main.js
+++ vite-package/vite-basic-vanilla2/main.js
@@ -1,5 +1,7 @@
+// 纯JS环境下使用Web Worker
 // const worker = new Worker('./worker.js');
 
+// Vite环境中使用Web Worker
 // 在后面一定要加?worker，这个资源才会被当作成是一个worker来引入的。
 import MyWorker from './worker?worker';
 const worker = new MyWorker();
