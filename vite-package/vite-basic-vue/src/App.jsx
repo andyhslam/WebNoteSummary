a777578118d28971ae7e81152d1ba8a25db3898f
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-03-10 23:16:06 +0800

    10-Vite静态资源引用

--- vite-package/vite-basic-vue/src/App.jsx
+++ vite-package/vite-basic-vue/src/App.jsx
@@ -5,12 +5,20 @@ import '@styles/index.css';
 import '@styles/stylus-test.styl';
 import { titleSize } from '@styles/example.module.css';
 
+// import logo from './assets/vue.svg?url';
+// import main from './main.js?raw';
+const imgUrl = new URL('./assets/vue.svg', import.meta.url).href;
+
+console.log(imgUrl);
+// console.log(main);
+
 export const Test = defineComponent({
   render() {
     return (
       <div className={`title ${titleSize}`}>
         hello vite jsx component
         <span>!!!</span>
+        <img src={imgUrl} alt="" />
       </div>
     );
   },
