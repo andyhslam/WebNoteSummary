ecbe6c18819e4b962bb9537b3063e80633ff2930
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-03-11 23:39:11 +0800

    13-Vite中导入json以及glob导入

---
+++ vite-package/vite-basic-vanilla2/main-old.js
@@ -0,0 +1,15 @@
+// 纯JS环境下使用Web Worker
+// const worker = new Worker('./worker.js');
+
+// Vite环境中使用Web Worker
+// 在后面一定要加?worker，这个资源才会被当作成是一个worker来引入的。
+import MyWorker from './worker?worker';
+const worker = new MyWorker();
+
+// 给worker定义onmessage事件，收取在子线程发过来的数据。
+worker.onmessage = function (ev) {
+  // console.log(ev.data);
+};
+
+import { text } from './json/data.json';
+console.log(text);
--- vite-package/vite-basic-vanilla2/main.js
+++ vite-package/vite-basic-vanilla2/main.js
@@ -1,12 +1,7 @@
-// 纯JS环境下使用Web Worker
-// const worker = new Worker('./worker.js');
+const modules = import.meta.glob('./glob/*[1].js');
 
-// Vite环境中使用Web Worker
-// 在后面一定要加?worker，这个资源才会被当作成是一个worker来引入的。
-import MyWorker from './worker?worker';
-const worker = new MyWorker();
-
-// 给worker定义onmessage事件，收取在子线程发过来的数据。
-worker.onmessage = function (ev) {
-  console.log(ev.data);
-};
+Object.entries(modules).forEach(([key, value]) => {
+  value().then((module) => {
+    console.log(key, module.default);
+  });
+});
