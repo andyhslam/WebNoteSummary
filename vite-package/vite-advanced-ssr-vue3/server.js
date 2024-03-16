d686e2efb6967815243438c16920129dc057b791
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-03-16 16:48:09 +0800

    18-02-服务端渲染SSR-Vue3案例-生产环境

--- vite-package/vite-advanced-ssr-vue3/server.js
+++ vite-package/vite-advanced-ssr-vue3/server.js
@@ -6,28 +6,59 @@ import { createServer as createViteServer } from 'vite';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-async function createServer() {
-  const app = express();
-  const vite = await createViteServer({
-    server: { middlewareMode: true },
-    appType: 'custom',
-  });
+async function createServer(isProd = process.env.NODE_ENV === 'production') {
+  const resolve = (p) => path.resolve(__dirname, p);
+  const port = process.env.PORT || 5173;
+  const base = process.env.BASE || '/';
+  const indexProd = isProd
+    ? fs.readFileSync(resolve('./dist/client/index.html'), 'utf-8')
+    : '';
+  // const ssrManifest = isProd
+  //   ? fs.readFileSync(resolve('./dist/client/.vite/ssr-manifest.json'), 'utf-8')
+  //   : undefined;
 
-  app.use(vite.middlewares);
+  const app = express();
+
+  let vite;
+  if (!isProd) {
+    vite = await createViteServer({
+      server: { middlewareMode: true },
+      appType: 'custom',
+      base,
+    });
+    app.use(vite.middlewares);
+  } else {
+    // 在生产环境只要设置静态目录
+    // 第一种方式
+    const serveStatic = (await import('serve-static')).default;
+    const distClient = serveStatic(resolve('dist/client'), {
+      index: false, // 不自动打开索引页面
+    });
+    app.use(distClient);
+
+    // 第二种方式参照 https://github.com/bluwy/create-vite-extra/blob/master/template-ssr-vue/server.js
+    // const compression = (await import('compression')).default;
+    // const sirv = (await import('sirv')).default;
+    // app.use(compression());
+    // app.use(base, sirv('./dist/client', { extensions: [] }));
+  }
+
   app.use('*', async (req, res) => {
     const url = req.originalUrl;
-
+    let template;
+    let render;
     try {
-      let template = fs.readFileSync(
-        path.resolve(__dirname, './index.html'),
-        'utf-8',
-      );
-      template = await vite.transformIndexHtml(url, template);
+      if (!isProd) {
+        template = fs.readFileSync(resolve('./index.html'), 'utf-8');
+        template = await vite.transformIndexHtml(url, template);
+        render = (await vite.ssrLoadModule('/src/entry-server.js')).render;
+      } else {
+        template = indexProd;
+        render = (await import('./dist/server/entry-server.js')).render;
+      }
 
-      const { render } = await vite.ssrLoadModule('/src/entry-server.js');
       const appHtml = await render(url);
       const html = template.replace('<!--ssr-outlet-->', appHtml);
-
       res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
     } catch (e) {
       vite.ssrFixStacktrace(e);
