3bc6a78856b151d681d46b53e3852eef867536e2
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-03-26 22:30:10 +0800

    vite中对css以及css模块化的简单处理

--- vite-package2/test-vite/main.js
+++ vite-package2/test-vite/main.js
@@ -1,5 +1,5 @@
-import { count } from './counter.js';
-import './request.js';
+import { count } from './src/utils.js/counter.js';
+import './src/utils.js/request.js';
 import './src/style/index.css';
 import './src/component/A.js';
 import './src/component/B.js';
