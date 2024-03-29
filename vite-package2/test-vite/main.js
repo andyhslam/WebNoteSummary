4c77fdf62c825b8a68123b30cb742feaa962db1e
andyhslam <42641473+andyhslam@users.noreply.github.com>
2024-03-29 08:14:06 +0800

    【补丁篇】关于为什么postcss没有编译全局css变量

--- vite-package2/test-vite/main.js
+++ vite-package2/test-vite/main.js
@@ -1,7 +1,8 @@
 import { count } from './src/utils.js/counter.js';
 import './src/utils.js/request.js';
+import './src/style/variable.css';
 import './src/style/index.css';
-import './src/style/index.less';
+// import './src/style/index.less';
 import './src/component/A.js';
 import './src/component/B.js';
 
--- vite-package2/test-vite/postcss.config.js
+++ vite-package2/test-vite/postcss.config.js
@@ -1,6 +1,12 @@
 // 该预设环境插件内置很多插件，它会一次性安装所有必要的插件(包括语法降级的插件、语法编译的插件等等)
 const postcssPresetEnv = require('postcss-preset-env');
+const path = require('path');
 
 module.exports = {
-  plugins: [postcssPresetEnv()],
+  plugins: [
+    postcssPresetEnv({
+      // 告知postcss需要记住的一些全局变量，不过该属性已经废弃
+      importFrom: path.resolve(__dirname, './src/style/variable.css'),
+    }),
+  ],
 };
